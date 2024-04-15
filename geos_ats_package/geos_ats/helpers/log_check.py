f7525462afb7106f37b54b40cdc4435a81012e0e
Christopher Sherman <sherman27@llnl.gov>
2024-04-15 10:32:20 -0700

    Adding yaml input option to geos_ats log checker

--- geos_ats_package/geos_ats/helpers/log_check.py
+++ geos_ats_package/geos_ats/helpers/log_check.py
@@ -1,19 +1,50 @@
 from configparser import ConfigParser
 import argparse
 import os
+import yaml
+import socket
 from typing import Iterable
 
 
-def log_check( fname: str, ignored: Iterable[ str ] ) -> None:
+def check_yaml( yaml_file: str ) -> Iterable[ str ]:
+    """
+    Check the integrated test yaml file for tests to ignore
+
+    Args:
+        yaml_file (str): Integrated test yaml file
+
+    Returns:
+        list: List of tests to ignore
+    """
+    hostname = socket.gethostname()
+    ignored_tests = []
+    with open( yaml_file ) as f:
+        test_options = yaml.safe_load( f )
+        allow_fail = test_options.get( 'allow_fail', {} )
+        for k, v in allow_fail.items():
+            if ( k in hostname ) or ( k == 'all' ):
+                if v:
+                    ignored_tests.extend( [ x.strip() for x in v.split( ',' ) ] )
+
+    return ignored_tests
+
+
+def log_check( fname: str, yaml_file: str, ignored: Iterable[ str ] ) -> None:
     """
     Check logs produced by geos_ats
 
     Args:
         fname (str): Path to geos_ats log
+        yaml_file (str): Integrated test yaml file
         ignored (list): List of test name failures to ignore
     """
     log = ConfigParser()
     log.read( os.path.expanduser( fname ) )
+
+    if yaml_file:
+        yaml_file = os.path.expanduser( yaml_file )
+        if os.path.isfile( yaml_file ):
+            ignored.extend( check_yaml( yaml_file ) )
 
     Nignore = 0
     ignore_names = []
@@ -70,6 +101,7 @@ def log_check( fname: str, ignored: Iterable[ str ] ) -> None:
 def main():
     parser = argparse.ArgumentParser( description="GEOS ATS Test Log Check" )
     parser.add_argument( "log_file", type=str, help="Path to the log file" )
+    parser.add_argument( "-y", "--yaml-file", type=str, help="Integrated test yaml file", default='' )
     parser.add_argument( "-i",
                          "--ignore-fail",
                          nargs='+',
@@ -77,7 +109,7 @@ def main():
                          action="extend",
                          help='Ignore specific tests when evaluating success' )
     args = parser.parse_args()
-    log_check( args.log_file, args.ignore_check )
+    log_check( args.log_file, args.yaml_file, args.ignore_fail )
 
 
 if __name__ == '__main__':
