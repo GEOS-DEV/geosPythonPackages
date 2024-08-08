f8d19d5670ce137f5cbf2bb1355ab5cd5e16a8fc
alexbenedicto <alexandre.benedicto@external.totalenergies.com>
2024-08-08 10:04:21 -0700

    Mesh stats completed with node sharing number and better parsing format.

--- geos-mesh/src/geos/mesh/doctor/checks/mesh_stats.py
+++ geos-mesh/src/geos/mesh/doctor/checks/mesh_stats.py
@@ -1,6 +1,6 @@
 import logging
-from typing import Union
 import numpy as np
+import numpy.typing as npt
 from dataclasses import dataclass
 
 from vtkmodules.util.numpy_support import (
@@ -15,25 +15,26 @@ from . import vtk_utils
 class Options:
     info: str
 
-np_hinting = Union[np.float32, np.float64, np.int32, np.int64]
+ArrayGeneric = npt.NDArray[np.generic]
 
 @dataclass( frozen=True )
 class MeshComponentData:
     componentType: str
-    scalar_names: list[str]
-    scalar_min_values: list[np_hinting]
-    scalar_max_values: list[np_hinting]
-    tensor_names: list[str]
-    tensor_min_values: list[np.array[np_hinting]]
-    tensor_max_values: list[np.array[np_hinting]]
+    scalar_names: list[ str ]
+    scalar_min_values: list[ np.generic ] # base class for all scalar types numpy
+    scalar_max_values: list[ np.generic ]
+    tensor_names: list[ str ]
+    tensor_min_values: list[ ArrayGeneric ]
+    tensor_max_values: list[ ArrayGeneric ]
 
 @dataclass( frozen=True )
 class Result:
-    number_elements: int
-    number_nodes: int
+    number_cells: int
+    number_points: int
     number_cell_types: int
-    cell_types: list[str]
-    cell_type_counts: list[int]
+    cell_types: list[ str ]
+    cell_type_counts: list[ int ]
+    sum_number_cells_per_nodes: dict[ int, int ]
     min_coords: np.ndarray
     max_coords: np.ndarray
     is_empty_point_global_ids: bool
@@ -41,9 +42,10 @@ class Result:
     point_data: MeshComponentData
     cell_data: MeshComponentData
 
+
 def get_cell_types_and_counts(
         mesh: vtkUnstructuredGrid
-    )-> tuple[int, int, list[str], list[int]]:
+    )-> tuple[ int, int, list[ str ], list[ int ] ]:
     """From an unstructured grid, collects the number of cells,
     the number of cell types, the list of cell types and the counts
     of each cell element.
@@ -56,21 +58,62 @@ def get_cell_types_and_counts(
         (number_cells, number_cell_types, cell_types, cell_type_counts)
     """
     number_cells: int = mesh.GetNumberOfCells()
-    number_cell_types: int = mesh.GetDistinctCellTypesArray().GetSize()
-    cell_types: list[str] = []
-    for cell_type in range(number_cell_types):
-        cell_types.append(vtk_utils.vtkid_to_string(mesh.GetCellType(cell_type)))
-
-    cell_type_counts: list[int] = [0]*number_cell_types
-    for cell in range(number_cells):
-        for cell_type in range(number_cell_types):
-            if vtk_utils.vtkid_to_string(mesh.GetCell(cell).GetCellType()) == cell_types[cell_type]:
-                cell_type_counts[cell_type] += 1
+    distinct_array_types = mesh.GetDistinctCellTypesArray()
+    number_cell_types: int = distinct_array_types.GetNumberOfTuples()
+    # Get the different cell types in the mesh
+    cell_types: list[ str ] = []
+    for cell_type in range( number_cell_types ):
+        cell_types.append( vtk_utils.vtkid_to_string( distinct_array_types.GetTuple( cell_type )[ 0 ] ) )
+    # Counts how many of each type are present
+    cell_type_counts: list[ int ] = [ 0 ] * number_cell_types
+    for cell in range( number_cells ):
+        for cell_type in range( number_cell_types ):
+            if vtk_utils.vtkid_to_string( mesh.GetCell( cell ).GetCellType() ) == cell_types[ cell_type ]:
+                cell_type_counts[ cell_type ] += 1
                 break
+    return ( number_cells, number_cell_types, cell_types, cell_type_counts )
 
-    return (number_cells, number_cell_types, cell_types, cell_type_counts)
 
-def get_coords_min_max(mesh: vtkUnstructuredGrid) -> tuple[np.ndarray]:
+def get_number_cells_per_nodes( mesh: vtkUnstructuredGrid ) -> dict[ int, int ]:
+    """Finds for each point_id the number of cells sharing that same node.
+
+    Args:
+        mesh (vtkUnstructuredGrid): An unstructured grid.
+
+    Returns:
+        dict[ int, int ]: { point_id0: 8, ..., point_idN: 4 }
+    """
+    number_cells_per_nodes: dict[ int, int ] = {}
+    for point_id in range( mesh.GetNumberOfPoints() ):
+        number_cells_per_nodes[ point_id ] = 0
+    for cell_id in range( mesh.GetNumberOfCells() ):
+        cell = mesh.GetCell( cell_id )
+        for v in range(cell.GetNumberOfPoints()):
+            point_id = cell.GetPointId( v )
+            number_cells_per_nodes[ point_id ] += 1
+    return number_cells_per_nodes
+
+
+def summary_number_cells_per_nodes( 
+        number_cells_per_nodes: dict[ int, int ] ) -> dict[ int, int ]:
+    """Obtain the number of nodes that have X number of cells.
+
+    Args:
+        number_cells_per_nodes (dict[ int, int ]): { point_id0: 8, ..., point_idN: 4 }
+
+    Returns:
+        dict[ int, int ]: Number of ce
+    """
+    unique_number_cells = set( [ value for value in number_cells_per_nodes.values() ] )
+    summary: dict[ int, int ] = {}
+    for unique_number in unique_number_cells:
+        summary[unique_number] = 0
+    for number_cells in number_cells_per_nodes.values():
+        summary[number_cells] += 1
+    return summary
+    
+
+def get_coords_min_max( mesh: vtkUnstructuredGrid ) -> tuple[ np.ndarray ]:
     """From an unstructured mesh, returns the coordinates of
     the minimum and maximum points.
 
@@ -80,10 +123,11 @@ def get_coords_min_max(mesh: vtkUnstructuredGrid) -> tuple[np.ndarray]:
     Returns:
         tuple[np.ndarray]: Min and Max coordinates.
     """
-    coords: np.ndarray = vtk_to_numpy(mesh.GetPoints().GetData())
-    min_coords: np.ndarray = coords.min(axis=0)
-    max_coords: np.ndarray = coords.max(axis=0)
-    return (min_coords, max_coords)
+    coords: np.ndarray = vtk_to_numpy( mesh.GetPoints().GetData() )
+    min_coords: np.ndarray = coords.min( axis=0 )
+    max_coords: np.ndarray = coords.max( axis=0 )
+    return ( min_coords, max_coords )
+
 
 def build_MeshComponentData(
         mesh: vtkUnstructuredGrid, componentType: str = "point"
@@ -97,7 +141,7 @@ def build_MeshComponentData(
     Returns:
         meshCD (MeshComponentData): Object that gathers data regarding a mesh component.
     """
-    if componentType not in ["point", "cell"]:
+    if componentType not in [ "point", "cell" ]:
         componentType = "point"
         logging.error( f"Invalid component type chosen to build MeshComponentData. Defaulted to point." )
     
@@ -106,44 +150,56 @@ def build_MeshComponentData(
     else:
         number_arrays_data = mesh.GetCellData().GetNumberOfArrays()
 
-    meshCD: MeshComponentData = MeshComponentData()
-    meshCD.componentType = componentType
-    for i in range(number_arrays_data):
+    scalar_names: list[ str ] = []
+    scalar_min_values: list[ np.generic ] = []
+    scalar_max_values: list[ np.generic ] = []
+    tensor_names: list[ str ] = []
+    tensor_min_values: list[ ArrayGeneric ] = []
+    tensor_max_values: list[ ArrayGeneric ] = []
+    for i in range( number_arrays_data ):
         if componentType == "point":
-            data_array = mesh.GetPointData().GetArray(i)
+            data_array = mesh.GetPointData().GetArray( i )
         else:
-            data_array = mesh.GetCellData().GetArray(i)
+            data_array = mesh.GetCellData().GetArray( i )
         data_array_name = data_array.GetName()
-        data_np_array = vtk_to_numpy(data_array)
+        data_np_array = vtk_to_numpy( data_array )
         if data_array.GetNumberOfComponents() == 1: # assumes scalar cell data for max and min
-            meshCD.scalar_names.append(data_array_name)
-            meshCD.scalar_max_values.append(data_np_array.max()) 
-            meshCD.scalar_min_values.append(data_np_array.min())
+            scalar_names.append( data_array_name )
+            scalar_max_values.append( data_np_array.max() ) 
+            scalar_min_values.append( data_np_array.min() )
         else:
-            meshCD.tensor_names.append(data_array_name)
-            meshCD.tensor_max_values.append(data_np_array.max(axis=0))
-            meshCD.tensor_min_values.append(data_np_array.min(axis=0))
+            tensor_names.append( data_array_name )
+            tensor_max_values.append( data_np_array.max(axis=0) )
+            tensor_min_values.append( data_np_array.min(axis=0) )
 
-    return meshCD
+    return MeshComponentData( componentType=componentType, scalar_names=scalar_names,
+        scalar_min_values=scalar_min_values, scalar_max_values=scalar_max_values,
+        tensor_names=tensor_names, tensor_min_values=tensor_min_values,
+        tensor_max_values=tensor_max_values )
+
 
 def __check( mesh: vtkUnstructuredGrid, options: Options ) -> Result:
-
     number_points: int = mesh.GetNumberOfPoints()
-    cells_info = get_cell_types_and_counts(mesh)
-    number_cells: int = cells_info[0]
-    number_cell_types: int = cells_info[1]
-    cell_types: int = cells_info[2]
-    cell_type_counts: int = cells_info[3]
-    min_coords, max_coords = get_coords_min_max(mesh)
-    point_ids: bool = not bool(mesh.GetPointData().GetGlobalIds())
-    cell_ids: bool = not bool(mesh.GetCellData().GetGlobalIds())
-    point_data: MeshComponentData = build_MeshComponentData(mesh, "point")
-    cell_data: MeshComponentData = build_MeshComponentData(mesh, "cell")
+    cells_info = get_cell_types_and_counts( mesh )
+    number_cells: int = cells_info[ 0 ]
+    number_cell_types: int = cells_info[ 1 ]
+    cell_types: int = cells_info[ 2 ]
+    cell_type_counts: int = cells_info[ 3 ]
+    number_cells_per_nodes: dict[ int, int ] = get_number_cells_per_nodes( mesh )
+    sum_number_cells_per_nodes: dict[ int, int ] = summary_number_cells_per_nodes( number_cells_per_nodes )
+    min_coords, max_coords = get_coords_min_max( mesh )
+    point_ids: bool = not bool( mesh.GetPointData().GetGlobalIds() )
+    cell_ids: bool = not bool( mesh.GetCellData().GetGlobalIds() )
+    point_data: MeshComponentData = build_MeshComponentData( mesh, "point" )
+    cell_data: MeshComponentData = build_MeshComponentData( mesh, "cell" )
 
     return Result( number_points=number_points, number_cells=number_cells, number_cell_types=number_cell_types,
-                   cell_types=cell_types, cell_type_counts=cell_type_counts, min_coords=min_coords, max_coords=max_coords,
+                   cell_types=cell_types, cell_type_counts=cell_type_counts,
+                   sum_number_cells_per_nodes=sum_number_cells_per_nodes,
+                   min_coords=min_coords, max_coords=max_coords,
                    is_empty_point_global_ids=point_ids, is_empty_cell_global_ids=cell_ids, 
-                   point_data=point_data, cell_data=cell_data)
+                   point_data=point_data, cell_data=cell_data )
+
 
 def check( vtk_input_file: str, options: Options ) -> Result:
     mesh = vtk_utils.read_mesh( vtk_input_file )
--- geos-mesh/src/geos/mesh/doctor/parsing/mesh_stats_parsing.py
+++ geos-mesh/src/geos/mesh/doctor/parsing/mesh_stats_parsing.py
@@ -1,6 +1,7 @@
 import logging
+from typing import Iterable
 
-from checks.mesh_stats import Options, Result
+from geos.mesh.doctor.checks.mesh_stats import Options, Result
 
 from . import MESH_STATS
 
@@ -14,36 +15,58 @@ def convert( parsed_options ) -> Options:
     return Options( info="test" )
     
 def display_results( options: Options, result: Result ):
-    logging.info( f"The mesh has {result.num_elements} elements and {result.num_nodes} nodes." )
-    logging.info( f"There are {result.num_cell_types} different types of cell in the mesh:" )
-    for i in range(result.num_cell_types):
-        logging.info( f"\t {result.cell_types[i]} \t ({result.cell_type_counts[i]} cells)" )
+    logging.info( f"The mesh has {result.number_cells} cells and {result.number_points} points." )
+    logging.info( f"There are {result.number_cell_types} different types of cell in the mesh:" )
+    for i in range(result.number_cell_types):
+        logging.info( f"\t{result.cell_types[i]}\t({result.cell_type_counts[i]} cells)" )
+
+    logging.info( "Number of nodes being shared between exactly N cells:" )
+    logging.info( "\tCells\tNodes" )
+    for number_cells_per_node, number_of_occurences in result.sum_number_cells_per_nodes.items():
+        logging.info( f"\t{number_cells_per_node}\t{number_of_occurences}" )
     
-    logging.info( f"The domain is contained in {result.min_coords[0]} <= x <= {result.max_coords[0]}")
-    logging.info( f"                           {result.min_coords[1]} <= y <= {result.max_coords[1]}")
-    logging.info( f"                           {result.min_coords[2]} <= z <= {result.max_coords[2]}")
+    logging.info( "The domain is contained in:" )
+    logging.info( f"\t{result.min_coords[ 0 ]} <= x <= {result.max_coords[ 0 ]}" )
+    logging.info( f"\t{result.min_coords[ 1 ]} <= y <= {result.max_coords[ 1 ]}" )
+    logging.info( f"\t{result.min_coords[ 2 ]} <= z <= {result.max_coords[ 2 ]}" )
 
     logging.info( f"Does the mesh have global point ids: {not result.is_empty_point_global_ids}" )
     logging.info( f"Does the mesh have global cell ids: {not result.is_empty_cell_global_ids}" )
 
+    space_size: int = 3
     logging.info( f"There are {len(result.cell_data.scalar_names)} scalar fields on the cells:" )
     for i in range(len(result.cell_data.scalar_names)):
-        logging.info(   f"\t {result.cell_data.scalar_names[i]}"
-                      + f" \t min = {result.cell_data.scalar_min_values[i]}"
-                      + f" \t max = {result.cell_data.scalar_max_values[i]}" )
+        logging.info(   f"\t{result.cell_data.scalar_names[i]}"
+                      + harmonious_spacing(result.cell_data.scalar_names, i, space_size)
+                      + f"min = {result.cell_data.scalar_min_values[i]}" + " " * space_size
+                    #   + harmonious_spacing(result.cell_data.scalar_min_values, i, space_size)
+                      + f"max = {result.cell_data.scalar_max_values[i]}" )
+
     logging.info( f"There are {len(result.cell_data.tensor_names)} vector/tensor fields on the cells:" )
     for i in range(len(result.cell_data.tensor_names)):
-        logging.info(   f"\t {result.cell_data.tensor_names[i]}"
-                      + f" \t min = {result.cell_data.tensor_min_values[i]}"
-                      + f" \t max = {result.cell_data.tensor_max_values[i]}" )
-    
+        logging.info(   f"\t{result.cell_data.tensor_names[i]}"
+                      + harmonious_spacing(result.cell_data.tensor_names, i, space_size)
+                      + f"min = {result.cell_data.tensor_min_values[i]}" + " " * space_size
+                    #   + harmonious_spacing(result.cell_data.tensor_min_values, i, space_size)
+                      + f"max = {result.cell_data.tensor_max_values[i]}" )
+
     logging.info( f"There are {len(result.point_data.scalar_names)} scalar fields on the points:" )
     for i in range(len(result.point_data.scalar_names)):
-        logging.info(   f"\t {result.point_data.scalar_names[i]}"
-                      + f" \t min = {result.point_data.scalar_min_values[i]}"
-                      + f" \t max = {result.point_data.scalar_max_values[i]}" )
+        logging.info(   f"\t{result.point_data.scalar_names[i]}"
+                      + harmonious_spacing(result.point_data.scalar_names, i, space_size)
+                      + f"min = {result.point_data.scalar_min_values[i]}" + " " * space_size
+                    #   + harmonious_spacing(result.point_data.scalar_min_values, i, space_size)
+                      + f"max = {result.point_data.scalar_max_values[i]}" )
+    
     logging.info( f"There are {len(result.point_data.tensor_names)} vector/tensor fields on the points:" )
     for i in range(len(result.point_data.tensor_names)):
-        logging.info(   f"\t {result.point_data.tensor_names[i]}"
-                      + f" \t min = {result.point_data.tensor_min_values[i]}"
-                      + f" \t max = {result.point_data.tensor_max_values[i]}" )
+        logging.info(   f"\t{result.point_data.tensor_names[i]}"
+                      + harmonious_spacing(result.point_data.tensor_names, i, space_size)
+                      + f"min = {result.point_data.tensor_min_values[i]}" + " " * space_size
+                    #   + harmonious_spacing(result.point_data.tensor_min_values, i, space_size)
+                      + f"   max = {result.point_data.tensor_max_values[i]}" )
+
+def harmonious_spacing( iterable_objs: Iterable[ Iterable ], indexIter: int, space_size: int=3 ) -> str:
+    longest_element: Iterable = max( iterable_objs, key=len )
+    ideal_space: int = len( longest_element ) - len( iterable_objs[ indexIter ] ) + space_size
+    return " " * ideal_space
