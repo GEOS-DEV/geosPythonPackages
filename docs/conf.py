140551b333452873145db6a47ca060f2a4f8d142
Martin LEMAY <l1021338@fr-vmx00109.main.glb.corp.local>
2025-03-28 11:10:32 +0100

    fix doc issues

--- docs/conf.py
+++ docs/conf.py
@@ -72,7 +72,7 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = [ u'_build', 'Thumbs.db', '.DS_Store', 'cmake/*' ]
+exclude_patterns = [ u'_build', 'Thumbs.db', '.DS_Store', 'cmake/*', '' ]
 
 todo_include_todos = True
 
