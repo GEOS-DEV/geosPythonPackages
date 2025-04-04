66b111757cfc79c962db27f8a43ca8845246f6e0
mlemayTTE <martin.lemay@external.totalenergies.com>
2025-04-04 10:08:12 +0200

    Linting and typing fix

--- geos-geomechanics/src/geos/geomechanics/processing/geomechanicsCalculatorFunctions.py
+++ geos-geomechanics/src/geos/geomechanics/processing/geomechanicsCalculatorFunctions.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # # SPDX-FileCopyrightText: Copyright 2023-2024 TotalEnergies.
 # SPDX-FileContributor: Alexandre Benedicto, Martin Lemay
+from typing import Any
 import numpy as np
 import numpy.typing as npt
 
@@ -30,7 +31,7 @@ def specificGravity( density: npt.NDArray[ np.float64 ], specificDensity: float 
 
     if abs( specificDensity ) < EPSILON:
         return np.full_like( density, np.nan )
-    return density / specificDensity
+    return ( density / specificDensity ).astype( float )
 
 
 # https://en.wikipedia.org/wiki/Elastic_modulus
@@ -625,7 +626,7 @@ def criticalPorePressure(
     stressVector: npt.NDArray[ np.float64 ],
     rockCohesion: float,
     frictionAngle: float = 0.0,
-) -> npt.NDArray[ np.float64 ]:
+) -> npt.NDArray[ np.floating[ Any ] ]:
     r"""Compute the critical pore pressure.
 
     Fracturing can occur in areas where Critical pore pressure is greater than
@@ -664,9 +665,9 @@ def criticalPorePressure(
         maximumPrincipalStress[ i ] = p1
 
     # assertion frictionAngle < np.pi/2., so sin(frictionAngle) != 1
-    cohesiveTerm: npt.NDArray[ np.float64 ] = ( rockCohesion * np.cos( frictionAngle ) /
-                                                ( 1 - np.sin( frictionAngle ) ) )
-    residualTerm: npt.NDArray[ np.float64 ] = ( 3 * minimumPrincipalStress - maximumPrincipalStress ) / 2.0
+    cohesiveTerm: npt.NDArray[ np.floating[ Any ] ] = ( rockCohesion * np.cos( frictionAngle ) /
+                                                        ( 1.0 - np.sin( frictionAngle ) ) )
+    residualTerm: npt.NDArray[ np.floating[ Any ] ] = ( 3.0 * minimumPrincipalStress - maximumPrincipalStress ) / 2.0
     return cohesiveTerm + residualTerm
 
 
@@ -848,16 +849,17 @@ def shearCapacityUtilization( traction: npt.NDArray[ np.float64 ], rockCohesion:
     assert traction.shape[ 1 ] == 3, "Traction vector must have 3 components."
 
     scu: npt.NDArray[ np.float64 ] = np.full( traction.shape[ 0 ], np.nan )
-    for i, tractionVec in enumerate( traction ):
+    for i in range( traction.shape[ 0 ] ):
+        tractionVec: npt.NDArray[ np.float64 ] = traction[ i ]
         # use -1 to agree with Geos convention (i.e., compression with negative stress)
-        stressNormal: npt.NDArray[ np.float64 ] = -1.0 * tractionVec[ 0 ]
+        stressNormal: float = -1.0 * tractionVec[ 0 ]
 
         # compute failure envelope
         mohrCoulomb: MohrCoulomb = MohrCoulomb( rockCohesion, frictionAngle )
         tauFailure: float = float( mohrCoulomb.computeShearStress( stressNormal ) )
         scu_i: float = np.nan
         if tauFailure > 0:
-            scu_i = np.abs( tractionVec[ 1 ] ) / tauFailure
+            scu_i = abs( tractionVec[ 1 ] ) / tauFailure
         # compute SCU
         scu[ i ] = scu_i
     return scu
