28ae96fabc949e4a15983596659bc4c25b2e2c02
Paloma Martinez <104762252+paloma-martinez@users.noreply.github.com>
2025-04-16 15:42:15 +0200

    Linting and typing

--- geos-mesh/tests/conftest.py
+++ geos-mesh/tests/conftest.py
@@ -12,30 +12,46 @@ import numpy.typing as npt
 from vtkmodules.vtkCommonDataModel import vtkDataSet
 from vtkmodules.vtkIOXML import vtkXMLUnstructuredGridReader
 
-import pytest
-
 
 @pytest.fixture
-def array(request: str) -> npt.NDArray:
-    data = np.load("data/data.npz")
+def array( request: str ) -> npt.NDArray:
+    """Fixture to get reference array depending on request array name.
 
-    return data[request.param]
+    Args:
+        request (str): _description_
+
+    Returns:
+        npt.NDArray: _description_
+    """
+    data = np.load( "data/data.npz" )
+
+    return data[ request.param ]
 
 
-
-
-@pytest.fixture(scope="function")
+@pytest.fixture( scope="function" )
 def vtkDataSetTest() -> vtkDataSet:
+    """Load vtk dataset to run the tests in test_vtkUtils.py.
+
+    Returns:
+        vtkDataSet: _description_
+    """
     reader: vtkXMLUnstructuredGridReader = vtkXMLUnstructuredGridReader()
     reader.SetFileName( "../../../GEOS/inputFiles/poromechanicsFractures/domain_res5_id.vtu" )
     reader.Update()
-    
+
     return reader.GetOutput()
 
 
+@pytest.fixture( scope="function" )
+def vtkdatasetWithComponentNames( vtkDataSetTest: vtkDataSet ) -> vtkDataSet:
+    """Set names for existing vtk dataset for test purpose.
 
-@pytest.fixture(scope="function")
-def vtkdatasetWithComponentNames(vtkDataSetTest: vtkDataSet):
+    Args:
+        vtkDataSetTest (vtkDataSet): _description_
+
+    Returns:
+        _type_: _description_
+    """
     attributeName1: str = "PERM"
 
     # return dataset
@@ -45,4 +61,3 @@ def vtkdatasetWithComponentNames(vtkDataSetTest: vtkDataSet):
         vtkDataSetTest.GetCellData().GetArray( attributeName1 ).SetComponentName( 2, "component3" )
 
     return vtkDataSetTest
-
--- geos-mesh/tests/test_vtkUtils.py
+++ geos-mesh/tests/test_vtkUtils.py
@@ -15,30 +15,45 @@ from vtk import (  # type: ignore[import-untyped]
 import geos.mesh.vtkUtils as vtkutils
 
 
+@pytest.mark.parametrize( "onpoints, expected", [ ( True, {
+    'GLOBAL_IDS_POINTS': 1
+} ), ( False, {
+    'CELL_MARKERS': 1,
+    'PERM': 3,
+    'PORO': 1,
+    'FAULT': 1,
+    'GLOBAL_IDS_CELLS': 1
+} ) ] )
+def test_getAttributesFromDataSet( vtkDataSetTest: vtkDataSet, onpoints: bool, expected: dict[ str, int ] ) -> None:
+    """Test getAttributesFromDataSet function.
 
-@pytest.mark.parametrize( "onpoints, expected", [
-    ( True, { 'GLOBAL_IDS_POINTS': 1 } ),
-    ( False, { 'CELL_MARKERS': 1, 'PERM': 3, 'PORO': 1, 'FAULT': 1, 'GLOBAL_IDS_CELLS': 1 } )
-])
-def test_getAttributesFromDataSet( vtkDataSetTest: vtkDataSet,
-                                  onpoints: bool, expected: dict[ str, int ]) -> None:
-    attributes: dict[ str, int ] = vtkutils.getAttributesFromDataSet( object=vtkDataSetTest,
-                                                                     onPoints=onpoints )
+    Args:
+        vtkDataSetTest (vtkDataSet): _description_
+        onpoints (bool): _description_
+        expected (dict[ str, int ]): _description_
+    """
+    attributes: dict[ str, int ] = vtkutils.getAttributesFromDataSet( object=vtkDataSetTest, onPoints=onpoints )
     assert attributes == expected
 
 
 @pytest.mark.parametrize( "attributeName, onpoints, expected", [
     ( "PORO", False, 1 ),
     ( "PORO", True, 0 ),
-])
-def test_isAttributeInObjectDataSet( vtkDataSetTest: vtkDataSet,
-                                    attributeName: str,
-                                    onpoints: bool, expected: bool ) -> None:
-    obtained: bool = vtkutils.isAttributeInObjectDataSet( object=vtkDataSetTest,
-                                                         attributeName=attributeName,
-                                                         onPoints=onpoints )
-    assert obtained == expected
+] )
+def test_isAttributeInObjectDataSet( vtkDataSetTest: vtkDataSet, attributeName: str, onpoints: bool,
+                                     expected: bool ) -> None:
+    """Test isAttributeFromDataSet function.
 
+    Args:
+        vtkDataSetTest (vtkDataSet): _description_
+        attributeName (str): _description_
+        onpoints (bool): _description_
+        expected (bool): _description_
+    """
+    obtained: bool = vtkutils.isAttributeInObjectDataSet( object=vtkDataSetTest,
+                                                          attributeName=attributeName,
+                                                          onPoints=onpoints )
+    assert obtained == expected
 
 
 @pytest.mark.parametrize( "attributeName, onpoints, expected", [
@@ -46,43 +61,71 @@ def test_isAttributeInObjectDataSet( vtkDataSetTest: vtkDataSet,
     ( "PERM", False, 3 ),
     ( "GLOBAL_IDS_POINTS", True, 1 ),
 ] )
-def test_getNumberOfComponentsDataSet( vtkDataSetTest: vtkDataSet,
+def test_getNumberOfComponentsDataSet(
+    vtkDataSetTest: vtkDataSet,
     attributeName: str,
     onpoints: bool,
-    expected: int, ) -> None:
-    obtained: int = vtkutils.getNumberOfComponentsDataSet( vtkDataSetTest, attributeName, onpoints)
+    expected: int,
+) -> None:
+    """Test getNumberOfComponentsDataSet function.
+
+    Args:
+        vtkDataSetTest (vtkDataSet): _description_
+        attributeName (str): _description_
+        onpoints (bool): _description_
+        expected (int): _description_
+    """
+    obtained: int = vtkutils.getNumberOfComponentsDataSet( vtkDataSetTest, attributeName, onpoints )
     assert obtained == expected
 
 
 @pytest.mark.parametrize( "attributeName, onpoints, expected", [
-    ( "PERM", False, ("component1", "component2", "component3")),
-    ( "PORO", False, ()),
+    ( "PERM", False, ( "component1", "component2", "component3" ) ),
+    ( "PORO", False, () ),
 ] )
 def test_getComponentNamesDataSet( vtkdatasetWithComponentNames: vtkDataSet, attributeName: str, onpoints: bool,
-                                  expected: tuple[ str, ...] ) -> None:
-    obtained : tuple[ str, ...] = vtkutils.getComponentNamesDataSet( vtkdatasetWithComponentNames, attributeName, onpoints)
+                                   expected: tuple[ str, ...] ) -> None:
+    """Test getComponentNamesDataSet function.
+
+    Args:
+        vtkdatasetWithComponentNames (vtkDataSet): _description_
+        attributeName (str): _description_
+        onpoints (bool): _description_
+        expected (tuple[ str, ...]): _description_
+    """
+    obtained: tuple[ str, ...] = vtkutils.getComponentNamesDataSet( vtkdatasetWithComponentNames, attributeName,
+                                                                    onpoints )
 
     assert obtained == expected
 
 
-@pytest.mark.parametrize("attributeName, dataType, expectedDatatypeArray", [
-    ( "test_double", VTK_DOUBLE, "vtkDoubleArray" ),
-    ( "test_float", VTK_FLOAT, "vtkFloatArray" ),
-    ( "test_int", VTK_INT, "vtkIntArray" ),
-    ( "test_unsigned_int", VTK_UNSIGNED_INT, "vtkUnsignedIntArray" ),
-    ( "test_char", VTK_CHAR, "vtkCharArray" ),
-    # ("testFail", 4566, pytest.fail) #TODO
-])
+@pytest.mark.parametrize(
+    "attributeName, dataType, expectedDatatypeArray",
+    [
+        ( "test_double", VTK_DOUBLE, "vtkDoubleArray" ),
+        ( "test_float", VTK_FLOAT, "vtkFloatArray" ),
+        ( "test_int", VTK_INT, "vtkIntArray" ),
+        ( "test_unsigned_int", VTK_UNSIGNED_INT, "vtkUnsignedIntArray" ),
+        ( "test_char", VTK_CHAR, "vtkCharArray" ),
+        # ("testFail", 4566, pytest.fail) #TODO
+    ] )
 def test_createEmptyAttribute(
     attributeName: str,
     dataType: int,
     expectedDatatypeArray: vtkDataArray,
 ) -> None:
-    componentNames: tuple[ str, str, str] =  ("d1, d2, d3")
-    newAttr: vtkDataArray = vtkutils.createEmptyAttribute(attributeName, componentNames, dataType)
+    """Test createEmptyAttribute function.
+
+    Args:
+        attributeName (str): _description_
+        dataType (int): _description_
+        expectedDatatypeArray (vtkDataArray): _description_
+    """
+    componentNames: tuple[ str, str, str ] = ( "d1, d2, d3" )
+    newAttr: vtkDataArray = vtkutils.createEmptyAttribute( attributeName, componentNames, dataType )
 
     assert newAttr.GetNumberOfComponents() == len( componentNames )
     assert newAttr.GetComponentName( 0 ) == componentNames[ 0 ]
     assert newAttr.GetComponentName( 1 ) == componentNames[ 1 ]
     assert newAttr.GetComponentName( 2 ) == componentNames[ 2 ]
-    assert newAttr.IsA(str(expectedDatatypeArray))
+    assert newAttr.IsA( str( expectedDatatypeArray ) )
