f85ae355cb8002cbd74dfd3c4ddc74203f1a2776
Jean Fechter <jean.fechter@kitware.com>
2025-05-05 11:53:18 +0200

    test: add properties checker test

---
+++ geos-trame/tests/test_properties_checker.py
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright 2023-2024 TotalEnergies.
+# SPDX-FileContributor: Kitware
+from pathlib import Path
+
+from geos_trame.app.core import GeosTrame
+from tests.trame_fixtures import trame_state, trame_server_layout
+
+
+def test_properties_checker(trame_server_layout, trame_state):
+    root_path = Path(__file__).parent.absolute().__str__()
+    file_name = root_path + "/data/singlePhaseFlow/FieldCaseTutorial3_smoke.xml"
+
+    geos_trame = GeosTrame(trame_server_layout[0], file_name)
+
+    field = trame_state.deck_tree[4]["children"][0]
+    assert field["valid"] == 0
+    geos_trame.properties_checker.check_field(field)
+    assert field["valid"] == 1
