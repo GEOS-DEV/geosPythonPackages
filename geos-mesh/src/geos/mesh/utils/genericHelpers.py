4b54e48a6a4a5afeb4111801570115358168d022
mlemayTTE <martin.lemay@external.totalenergies.com>
2025-05-22 11:45:48 +0200

    linting fix

--- geos-mesh/src/geos/mesh/utils/genericHelpers.py
+++ geos-mesh/src/geos/mesh/utils/genericHelpers.py
@@ -10,7 +10,6 @@ from vtkmodules.vtkCommonDataModel import vtkUnstructuredGrid, vtkMultiBlockData
 from vtkmodules.vtkFiltersCore import vtk3DLinearGridPlaneCutter
 from geos.mesh.utils.multiblockHelpers import ( getBlockElementIndexesFlatten, getBlockFromFlatIndex )
 
-
 __doc__ = """
 Generic VTK utilities.
 
