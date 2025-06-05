54c3cdf68e3c0adff27d4849039d1d6d07727589
alexbenedicto <alexandre.benedicto@external.totalenergies.com>
2025-06-04 17:59:48 -0700

    First version of all_checks without "supported_elements" check

--- geos-mesh/src/geos/mesh/doctor/actions/all_checks.py
+++ geos-mesh/src/geos/mesh/doctor/actions/all_checks.py
@@ -2,25 +2,27 @@ from dataclasses import dataclass
 from geos.mesh.doctor.register import __load_module_action
 from geos.utils.Logger import getLogger
 
-logger = getLogger()
+logger = getLogger( "All_checks" )
 
 
 @dataclass( frozen=True )
 class Options:
     checks_to_perform: list[ str ]
-    checks_options: list
+    checks_options: dict[ str, any ]
+    check_displays: dict[ str, any ]
 
 
 @dataclass( frozen=True )
 class Result:
-    check_results: dict
+    check_results: dict[ str, any ]
 
 
 def action( vtk_input_file: str, options: Options ) -> list[ Result ]:
     check_results = dict()
-    for check, option in zip( options.checks_to_perform, options.checks_options ):
-        check_action = __load_module_action( check )
-        logger.info( f"Performing check '{check}'." )
+    for check_name in options.checks_to_perform:
+        check_action = __load_module_action( check_name )
+        logger.info( f"Performing check '{check_name}'." )
+        option = options.checks_options[ check_name ]
         check_result = check_action( vtk_input_file, option )
-        check_results[ check ] = check_result
+        check_results[ check_name ] = check_result
     return Result( check_results=check_results )
--- geos-mesh/src/geos/mesh/doctor/parsing/all_checks_parsing.py
+++ geos-mesh/src/geos/mesh/doctor/parsing/all_checks_parsing.py
@@ -1,114 +1,215 @@
+import argparse  # Assuming argparse is used for fill_subparser
 from copy import deepcopy
-from geos.mesh.doctor.checks.all_checks import Options, Result
-from geos.mesh.doctor.parsing import ( ALL_CHECKS, COLLOCATES_NODES, ELEMENT_VOLUMES, NON_CONFORMAL,
-                                       SELF_INTERSECTING_ELEMENTS, SUPPORTED_ELEMENTS )
-from geos.mesh.doctor.parsing.collocated_nodes_parsing import ( __COLLOCATED_NODES_DEFAULT, Options as OptionsCN, Result
-                                                                as ResultCN )
-from geos.mesh.doctor.parsing.element_volumes_parsing import ( __ELEMENT_VOLUMES_DEFAULT, Options as OptionsEV, Result
-                                                               as ResultEV )
-from geos.mesh.doctor.parsing.non_conformal_parsing import ( __NON_CONFORMAL_DEFAULT, Options as OptionsNC, Result as
-                                                             ResultNC )
-from geos.mesh.doctor.parsing.self_intersecting_elements_parsing import ( __SELF_INTERSECTING_ELEMENTS_DEFAULT, Options
-                                                                          as OptionsSIE, Result as ResultSIE )
-from geos.mesh.doctor.parsing.supported_elements_parsing import ( __SUPPORTED_ELEMENTS_DEFAULT, Options as OptionsSE,
-                                                                  Result as ResultSE )
+from dataclasses import dataclass
+from typing import Type
+
+# Assuming these are the container classes for all checks
+from geos.mesh.doctor.actions.all_checks import Options as AllChecksOptions
+from geos.mesh.doctor.actions.all_checks import Result as AllChecksResult
+
+# Import constants for check names
+from geos.mesh.doctor.parsing import (
+    ALL_CHECKS,  # Name for the subparser
+    COLLOCATES_NODES,
+    ELEMENT_VOLUMES,
+    NON_CONFORMAL,
+    SELF_INTERSECTING_ELEMENTS,
+    # SUPPORTED_ELEMENTS,
+)
+
+# Import module-specific Options, Result, and defaults
+# Using module aliases for clarity
+from geos.mesh.doctor.parsing import collocated_nodes_parsing as cn_parser
+from geos.mesh.doctor.parsing import element_volumes_parsing as ev_parser
+from geos.mesh.doctor.parsing import non_conformal_parsing as nc_parser
+from geos.mesh.doctor.parsing import self_intersecting_elements_parsing as sie_parser
+# from geos.mesh.doctor.parsing import supported_elements_parsing as se_parser
+from geos.mesh.doctor.parsing.cli_parsing import parse_comma_separated_string
 from geos.utils.Logger import getLogger
 
-__CHECK_ONLY_FEATURES = [
-    COLLOCATES_NODES, ELEMENT_VOLUMES, NON_CONFORMAL, SELF_INTERSECTING_ELEMENTS, SUPPORTED_ELEMENTS
+logger = getLogger( "All_checks_parsing" )
+
+# --- Centralized Configuration for Check Features ---
+# This structure makes it easier to manage checks and their properties.
+
+
+@dataclass( frozen=True )  # Consider using dataclass if appropriate, or a simple dict
+class CheckFeature:
+    name: str
+    options_cls: Type[ any ]  # Specific Options class (e.g., cn_parser.Options)
+    result_cls: Type[ any ]  # Specific Result class (e.g., cn_parser.Result)
+    default_params: dict[ str, any ]  # Parser keywords with default values
+    display: Type[ any ]  # Specific display function for results
+
+
+# Deepcopy to prevent accidental modification of originals default parameters
+CHECK_FEATURES_CONFIG = {
+    COLLOCATES_NODES:
+    CheckFeature( name=COLLOCATES_NODES,
+                  options_cls=cn_parser.Options,
+                  result_cls=cn_parser.Result,
+                  default_params=deepcopy( cn_parser.__COLLOCATED_NODES_DEFAULT ),
+                  display=cn_parser.display_results ),
+    ELEMENT_VOLUMES:
+    CheckFeature( name=ELEMENT_VOLUMES,
+                  options_cls=ev_parser.Options,
+                  result_cls=ev_parser.Result,
+                  default_params=deepcopy( ev_parser.__ELEMENT_VOLUMES_DEFAULT ),
+                  display=ev_parser.display_results ),
+    NON_CONFORMAL:
+    CheckFeature( name=NON_CONFORMAL,
+                  options_cls=nc_parser.Options,
+                  result_cls=nc_parser.Result,
+                  default_params=deepcopy( nc_parser.__NON_CONFORMAL_DEFAULT ),
+                  display=nc_parser.display_results ),
+    SELF_INTERSECTING_ELEMENTS:
+    CheckFeature( name=SELF_INTERSECTING_ELEMENTS,
+                  options_cls=sie_parser.Options,
+                  result_cls=sie_parser.Result,
+                  default_params=deepcopy( sie_parser.__SELF_INTERSECTING_ELEMENTS_DEFAULT ),
+                  display=sie_parser.display_results ),
+    # SUPPORTED_ELEMENTS:
+    # CheckFeature( name=SUPPORTED_ELEMENTS,
+    #               options_cls=se_parser.Options,
+    #               result_cls=se_parser.Result,
+    #               default_params=deepcopy( se_parser.__SUPPORTED_ELEMENTS_DEFAULT ),
+    #               display=se_parser.display_results ),
+}
+
+# Ordered list of check names, defining the default order and for consistent help messages
+ORDERED_CHECK_NAMES: list[ str ] = [
+    COLLOCATES_NODES,
+    ELEMENT_VOLUMES,
+    NON_CONFORMAL,
+    SELF_INTERSECTING_ELEMENTS,
+    # SUPPORTED_ELEMENTS,
 ]
-__CHECK_ONLY_FEATURES_DEFAULT = {
-    COLLOCATES_NODES: __COLLOCATED_NODES_DEFAULT,
-    ELEMENT_VOLUMES: __ELEMENT_VOLUMES_DEFAULT,
-    NON_CONFORMAL: __NON_CONFORMAL_DEFAULT,
-    SELF_INTERSECTING_ELEMENTS: __SELF_INTERSECTING_ELEMENTS_DEFAULT,
-    SUPPORTED_ELEMENTS: __SUPPORTED_ELEMENTS_DEFAULT
-}
-__CHECK_ONLY_FEATURES_OPTIONS = {
-    COLLOCATES_NODES: OptionsCN,
-    ELEMENT_VOLUMES: OptionsEV,
-    NON_CONFORMAL: OptionsNC,
-    SELF_INTERSECTING_ELEMENTS: OptionsSIE,
-    SUPPORTED_ELEMENTS: OptionsSE
-}
-__CHECK_ONLY_FEATURES_RESULTS = {
-    COLLOCATES_NODES: ResultCN,
-    ELEMENT_VOLUMES: ResultEV,
-    NON_CONFORMAL: ResultNC,
-    SELF_INTERSECTING_ELEMENTS: ResultSIE,
-    SUPPORTED_ELEMENTS: ResultSE
+DEFAULT_PARAMS: dict[ str, dict[ str, float ] ] = {
+    name: feature.default_params.copy() for name, feature in CHECK_FEATURES_CONFIG.items()
 }
 
-__CHECKS_TO_DO = "checks"
-__CHECKS_TO_DO_DEFAULT = __CHECK_ONLY_FEATURES
+# --- Argument Parser Constants ---
+CHECKS_TO_DO_ARG = "checks_to_perform"
+PARAMETERS_ARG = "set_parameters"
 
-__CHECKS_SET_PARAMETERS = "set_parameters"
-__CHECKS_SET_PARAMETERS_DEFAULT: list[ str ] = list()
-__CHECKS_SET_PARAMETERS_DEFAULT_HELP: str = ""
-for feature, default_map in __CHECK_ONLY_FEATURES_DEFAULT.items():
-    __CHECKS_SET_PARAMETERS_DEFAULT_HELP += f"For {feature},"
-    for name, value in default_map.items():
-        __CHECKS_SET_PARAMETERS_DEFAULT.append( name + ":" + str( value ) )
-        __CHECKS_SET_PARAMETERS_DEFAULT_HELP += " " + name + ":" + str( value )
-    __CHECKS_SET_PARAMETERS_DEFAULT_HELP += ". "
-
-logger = getLogger( "All_checks parsing" )
+# Generate help text for set_parameters dynamically
+PARAMETERS_ARG_HELP: str = ""
+for check_name in ORDERED_CHECK_NAMES:
+    config = CHECK_FEATURES_CONFIG[ check_name ]
+    if config.default_params:
+        config_params: list[ str ] = list()
+        for name, value in config.default_params.items():
+            config_params.append( f"{name}:{value}" )
+        PARAMETERS_ARG_HELP += f"For {check_name}: {', '.join( config_params )}. "
 
 
-def fill_subparser( subparsers ) -> None:
-    p = subparsers.add_parser(
-        ALL_CHECKS, help="Perform one or multiple mesh-doctor check operation in one command line on a same mesh." )
-    p.add_argument(
-        '--' + __CHECKS_TO_DO,
-        type=float,
-        metavar=", ".join( __CHECKS_TO_DO_DEFAULT ),
-        default=", ".join( __CHECKS_TO_DO_DEFAULT ),
-        required=False,
-        help="[list of comma separated str]: Name of the mesh-doctor checks that you want to perform on your mesh."
-        f" By default, all the checks will be performed which correspond to this list: \"{','.join(__CHECKS_TO_DO_DEFAULT)}\"."
-        f" If only two of these checks are needed, you can only select them by specifying: --{__CHECKS_TO_DO} {__CHECKS_TO_DO_DEFAULT[0]}, {__CHECKS_TO_DO_DEFAULT[1]}"
+# --- Argument Parser Setup ---
+def fill_subparser( subparsers: argparse._SubParsersAction ) -> None:
+    """Fills the subparser for 'ALL_CHECKS' with its arguments."""
+    parser = subparsers.add_parser(
+        ALL_CHECKS,
+        help="Perform one or multiple mesh-doctor check operations in one command line on the same mesh.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter  # Shows defaults in help
     )
-    p.add_argument(
-        '--' + __CHECKS_SET_PARAMETERS,
-        type=float,
-        metavar=", ".join( __CHECKS_SET_PARAMETERS_DEFAULT ),
-        default=", ".join( __CHECKS_SET_PARAMETERS_DEFAULT ),
+    parser.add_argument( f"--{CHECKS_TO_DO_ARG}",
+                         type=str,
+                         default="",
+                         required=False,
+                         help=( "Comma-separated list of mesh-doctor checks to perform. If no input was given, all of"
+                                f" the following checks will be executed by default: {ORDERED_CHECK_NAMES}. If you want"
+                                " to choose only certain of them, you can name them individually."
+                                f" Example: --{CHECKS_TO_DO_ARG} {ORDERED_CHECK_NAMES[0]},{ORDERED_CHECK_NAMES[1]}" ) )
+    parser.add_argument(
+        f"--{PARAMETERS_ARG}",
+        type=str,
+        default="",
         required=False,
-        help=
-        "[list of comma separated str]: Each of the checks that will be performed have some parameters to specify when using them."
-        " By default, every of these parameters have been set to default values that will be mostly used in the vast majority of cases."
-        f" If you want to change the values, just type the following command: --{__CHECKS_SET_PARAMETERS} parameter0:10, parameter1:25, ..."
-        f" The complete list of parameters to change with their default value is the following: {__CHECKS_SET_PARAMETERS_DEFAULT_HELP}"
-    )
+        help=( "Comma-separated list of parameters to set for the checks (e.g., 'param_name:value'). "
+               "These parameters override the defaults. Default parameters are:"
+               f" {PARAMETERS_ARG_HELP} Example: --{PARAMETERS_ARG} parameter_name:10.5,other_param:25" ) )
 
 
-def convert( parsed_options ) -> Options:
-    # first, we need to gather every check that will be performed
-    checks_to_do: list[ str ] = parsed_options[ __CHECKS_TO_DO ].replace( " ", "" ).split( "," )
-    checks_to_perform = set()
-    for check in checks_to_do:
-        if check not in __CHECKS_TO_DO_DEFAULT:
-            logger.critical( f"The given check '{check}' does not exist. Cannot perform this check. Choose between"
-                             f" the available checks: {__CHECKS_TO_DO_DEFAULT}." )
-        else:
-            checks_to_perform.add( check )
-    checks_to_perform = list( checks_to_perform )  # only unique checks because of set
-    # then, we need to find the values to set in the Options object of every check
-    set_parameters: list[ str ] = parsed_options[ __CHECKS_SET_PARAMETERS ].replace( " ", "" ).split( "," )
-    set_parameters_tuple: list[ tuple[ str ] ] = [ p.split( ":" ) for p in set_parameters ]
-    checks_parameters = deepcopy( __CHECK_ONLY_FEATURES_DEFAULT )
-    for set_param in set_parameters_tuple:
-        for default_parameters in checks_parameters.values():
-            if set_param[ 0 ] in default_parameters:
-                default_parameters[ set_param[ 0 ] ] = float( set_param[ 1 ] )
-    # finally, we can create the Options object for every check with the right parameters
-    checks_options = list()
-    for check_to_perform in checks_to_perform:
-        option_to_use = __CHECK_ONLY_FEATURES_OPTIONS[ check_to_perform ]
-        options_parameters: dict[ str, float ] = checks_parameters[ check_to_perform ]
-        checks_options.append( option_to_use( **options_parameters ) )
-    return Options( checks_to_perform=checks_to_perform, checks_options=checks_options )
+def convert( parsed_args: argparse.Namespace ) -> AllChecksOptions:
+    """
+    Converts parsed command-line arguments into an AllChecksOptions object.
+    """
+    # 1. Determine which checks to perform
+    final_selected_check_names: list[ str ] = deepcopy( ORDERED_CHECK_NAMES )
+    if not parsed_args[ CHECKS_TO_DO_ARG ]:  # handles default and if user explicitly provides --checks_to_perform ""
+        logger.info( "All current available checks in mesh-doctor will be performed." )
+    else:  # the user specifically entered check names to perform
+        checks_to_do: list[ str ] = parse_comma_separated_string( parsed_args[ CHECKS_TO_DO_ARG ] )
+        final_selected_check_names = list()
+        for name in checks_to_do:
+            if name not in CHECK_FEATURES_CONFIG:
+                logger.warning( f"The given check '{name}' does not exist. Cannot perform this check."
+                                f" Choose from: {ORDERED_CHECK_NAMES}." )
+            elif name not in final_selected_check_names:  # Add if valid and not already added
+                final_selected_check_names.append( name )
+
+        # If after parsing, no valid checks are selected (e.g., all inputs were invalid)
+        if not final_selected_check_names:
+            logger.error( "No valid checks selected based on input. No operations will be configured." )
+            raise ValueError( "No valid checks selected based on input. No operations will be configured." )
+
+    # 2. Prepare parameters of Options for every check feature that will be used
+    final_selected_check_params: dict[ str, dict[ str, float ] ] = deepcopy( DEFAULT_PARAMS )
+    for name in list( final_selected_check_params.keys() ):
+        if name not in final_selected_check_names:
+            del final_selected_check_params[name]  # Remove non-used check features
+
+    if not parsed_args[ PARAMETERS_ARG ]:  # handles default and if user explicitly provides --set_parameters ""
+        logger.info( "Default configuation of parameters adopted for every check to perform." )
+    else:
+        set_parameters = parse_comma_separated_string( parsed_args[ PARAMETERS_ARG ] )
+        for param in set_parameters:
+            if ':' not in param:
+                logger.warning( f"Parameter '{param}' in --{PARAMETERS_ARG} is not in 'name:value' format. Skipping." )
+                continue
+            name, *value = param.split( ':', 1 )
+            name = name.strip()
+            if value:  # Check if there is anything after the first colon
+                value_str = value[ 0 ].strip()
+            else:
+                # Handle cases where there's nothing after the colon, if necessary
+                logger.warning( f"Parameter '{name}' has no value after the colon. Skipping or using default." )
+                continue
+            try:
+                value_float = float( value_str )
+            except ValueError:
+                logger.warning(
+                    f"Invalid value for parameter '{name}': '{value_str}'. Must be a number. Skipping this override." )
+                continue
+
+            for check_name_key in final_selected_check_params.keys():  # Iterate through all possible checks
+                if param in final_selected_check_params[ check_name_key ]:
+                    final_selected_check_params[ check_name_key ][ param ] = value_float
+                    break
+
+    # 3. Instantiate the Options objects for the selected checks using their effective parameters
+    individual_check_options: dict[ str, any ] = dict()
+    individual_check_display: dict[ str, any ] = dict()
+    for check_name in list( final_selected_check_params.keys() ):
+        options_constructor_params = final_selected_check_params[ check_name ]
+        feature_config = CHECK_FEATURES_CONFIG[ check_name ]
+        try:
+            individual_check_options[ check_name ] = feature_config.options_cls( **options_constructor_params )
+            individual_check_display[ check_name ] = feature_config.display
+        except Exception as e:  # Catch potential errors during options instantiation
+            logger.error(
+                f"Failed to create options for check '{check_name}' with params {options_constructor_params}: {e}."
+                f" Therefore the check '{check_name}' will not be performed." )
+            final_selected_check_names.remove( check_name )
+
+    return AllChecksOptions( checks_to_perform=final_selected_check_names,
+                             checks_options=individual_check_options,
+                             check_displays=individual_check_display )
 
 
-def display_results( options: Options, result: Result ):
-    pass
+# --- Display Results ---
+def display_results( options: AllChecksOptions, result: AllChecksResult ) -> None:
+    """Displays the results of the checks."""
+    # Implementation for displaying results based on the structured options and results.
+    logger.info( f"Displaying results for checks: {options.checks_to_perform}" )
+    for name, res in result.check_results.items():
+        options.check_displays[ name ]( options.checks_options[ name ], res )
