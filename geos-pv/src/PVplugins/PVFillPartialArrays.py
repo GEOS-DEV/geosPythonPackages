35c3b74273fe665856424360c3d3c929fdabf35a
Romain Baville <romain.baville@external.totalenergies.com>
2025-06-16 17:20:33 +0200

    fixing bugs by removing the use of the abstract class

--- geos-pv/src/PVplugins/PVFillPartialArrays.py
+++ geos-pv/src/PVplugins/PVFillPartialArrays.py
@@ -8,11 +8,16 @@ from typing_extensions import Self
 
 
 from paraview.util.vtkAlgorithm import (  # type: ignore[import-not-found]
-    smdomain, smhint, smproperty, smproxy,
+    VTKPythonAlgorithmBase, smdomain, smhint, smproperty, smproxy,
 )
 
 from vtkmodules.vtkCommonDataModel import (
     vtkMultiBlockDataSet,
+)
+
+from vtkmodules.vtkCommonCore import (
+    vtkInformation,
+    vtkInformationVector,
 )
 
 # update sys.path to load all GEOS Python Package dependencies
@@ -23,7 +28,6 @@ from geos.pv.utils.config import update_paths
 update_paths()
 
 from geos.mesh.processing.FillPartialArrays import FillPartialArrays
-from geos.pv.utils.AbstractPVPluginVtkWrapper import AbstractPVPluginVtkWrapper
 
 __doc__ = """
 Fill partial arrays of input mesh.
@@ -47,11 +51,11 @@ To use it:
     dataTypes=[ "vtkMultiBlockDataSet" ],
     composite_data_supported=True,
 )
-class PVFillPartialArrays( AbstractPVPluginVtkWrapper ):
+class PVFillPartialArrays( VTKPythonAlgorithmBase ):
 
-    def __init__( self: Self ) -> None:
+    def __init__( self: Self,) -> None:
         """Map the properties of a server mesh to a client mesh."""
-        super().__init__()
+        super().__init__(nInputPorts=1, nOutputPorts=1, inputType="vtkMultiBlockDataSet", outputType="vtkMultiBlockDataSet")
 
         self._clearSelectedAttributeMulti: bool = True
         self._selectedAttributeMulti: list[ str ] = []
@@ -95,19 +99,56 @@ class PVFillPartialArrays( AbstractPVPluginVtkWrapper ):
         self._selectedAttributeMulti.append( name )
         self.Modified()
 
-    def applyVtkFilter(
+    def RequestDataObject(
         self: Self,
-        input: vtkMultiBlockDataSet,
-    ) -> vtkMultiBlockDataSet:
-        """Apply vtk filter.
+        request: vtkInformation,
+        inInfoVec: list[ vtkInformationVector ],
+        outInfoVec: vtkInformationVector,
+    ) -> int:
+        """Inherited from VTKPythonAlgorithmBase::RequestDataObject.
 
         Args:
-            input (vtkMultiBlockDataSet): input mesh
+            request (vtkInformation): Request
+            inInfoVec (list[vtkInformationVector]): Input objects
+            outInfoVec (vtkInformationVector): Output objects
 
         Returns:
-            vtkMultiBlockDataSet: output mesh
+            int: 1 if calculation successfully ended, 0 otherwise.
         """
+        inData = self.GetInputData( inInfoVec, 0, 0 )
+        outData = self.GetOutputData( outInfoVec, 0 )
+        assert inData is not None
+        if outData is None or ( not outData.IsA( inData.GetClassName() ) ):
+            outData = inData.NewInstance()
+            outInfoVec.GetInformationObject( 0 ).Set( outData.DATA_OBJECT(), outData )
+        return super().RequestDataObject( request, inInfoVec, outInfoVec )  # type: ignore[no-any-return]
+
+    def RequestData(
+        self: Self,
+        request: vtkInformation,  # noqa: F841
+        inInfoVec: list[ vtkInformationVector ],
+        outInfoVec: vtkInformationVector,
+    ) -> int:
+        """Inherited from VTKPythonAlgorithmBase::RequestData.
+
+        Args:
+            request (vtkInformation): request
+            inInfoVec (list[vtkInformationVector]): input objects
+            outInfoVec (vtkInformationVector): output objects
+
+        Returns:
+            int: 1 if calculation successfully ended, 0 otherwise.
+        """
+        inputMesh: vtkMultiBlockDataSet = self.GetInputData( inInfoVec, 0, 0 )
+        outputMesh: vtkMultiBlockDataSet = self.GetOutputData( outInfoVec, 0 )
+        assert inputMesh is not None, "Input server mesh is null."
+        assert outputMesh is not None, "Output pipeline is null."
+        
         filter: FillPartialArrays = FillPartialArrays()
-        filter.SetInputDataObject( input )
+        filter.SetSelectedAttributeMulti( self._selectedAttributeMulti )
+        filter.SetInputDataObject( inputMesh )
         filter.Update()
-        return filter.GetOutputDataObject( 0 )
+        outputMesh.ShallowCopy( filter.GetOutputDataObject( 0 ) )
+
+        self._clearSelectedAttributeMulti = True
+        return 1
