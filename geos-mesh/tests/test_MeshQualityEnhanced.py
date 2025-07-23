Remove tetraVolume.vtu test file and fix tests results due to bug in filter computation

geos-mesh/tests/test_MeshQualityEnhanced.py
@@ -1,17 +1,18 @@
-# SPDX-FileContributor: Martin Lemay
+# SPDX-FileContributor: Martin Lemay, Paloma Martinez
 # SPDX-License-Identifier: Apache 2.0
 # ruff: noqa: E402 # disable Module level import not at top of file
 import os
 from matplotlib.figure import Figure
 from dataclasses import dataclass
 import numpy as np
+import numpy.typing as npt
 import pandas as pd
 import pytest
 from typing import (
     Iterator,
     Optional,
 )
-
+from geos.mesh.utils.genericHelpers import createMultiCellMesh
 from geos.mesh.processing.meshQualityMetricHelpers import (
     getAllCellTypesExtended, )
 from geos.mesh.stats.MeshQualityEnhanced import MeshQualityEnhanced
@@ -22,10 +23,11 @@ from vtkmodules.vtkCommonDataModel import ( vtkUnstructuredGrid, vtkCellData, vt
                                             VTK_QUAD, VTK_TETRA, VTK_PYRAMID, VTK_WEDGE, VTK_HEXAHEDRON )
 from vtkmodules.vtkIOXML import vtkXMLUnstructuredGridReader
 
+
 # input data
 meshName_all: tuple[ str, ...] = (
     "polydata",
-    "tetraVolume",
+    "tetra_mesh",
 )
 cellTypes_all: set[ int ] = ( VTK_TRIANGLE, VTK_TETRA )
 qualityMetrics_all: tuple[ set[ int ], ...] = (
@@ -47,17 +49,18 @@ cellTypeCounts_all: tuple[ tuple[ int, ...], ...] = ( (
 ), (
     0,
     0,
-    368606,
+    8,
     0,
     0,
     0,
     0,
-    368606,
+    8,
 ) )
 metricsSummary_all = (
-    ( ( 1.07, 0.11, 1.00, 1.94, 26324.0 ), ( 1.07, 0.11, 1.00, 1.94, 26324.0 ), ( 64.59, 6.73, 60.00, 110.67,
+    ( ( 1.07, 0.11, 1.0, 1.94, 26324.0 ), ( 0.91, 0.1, 0.53, 1.0, 26324.0 ), ( 64.59, 6.73, 60.00, 110.67,
                                                                                   26324.0 ) ),
-    ( ( 1.71, 0.32, 1.02, 3.3, 368606.0 ), ( 1.71, 0.32, 1.02, 3.3, 368606.0 ), ( 0.65, 0.15, 0.05, 0.94, 368606.0 ) ),
+                                                                                
+    ( ( -0.28, 0.09, -0.49, -0.22, 8.0 ), ( 0.7, 0.1, 0.47, 0.79, 8.0 ), ( 0.8, 0.12, 0.58, 0.95, 8.0 ) ),
 )
 
 
@@ -66,11 +69,48 @@ class TestCase:
     """Test case."""
     __test__ = False
     #: mesh
-    meshName: str
+    mesh: vtkUnstructuredGrid
     cellType: vtkCellTypes
     qualityMetrics: set[ int ]
     cellTypeCounts: tuple[ int ]
     metricsSummary: tuple[ float ]
+
+def __get_tetra_dataset() -> vtkUnstructuredGrid:
+    """Extract tetrahedra dataset from csv and add some deformations."""
+    # Get tetra mesh
+    data_root: str = os.path.join( os.path.dirname( os.path.abspath( __file__ ) ), "data" )
+    filename: str = "tetra_mesh.csv"
+    nbPtsCell: int = 4
+
+    ptsCoord: npt.NDArray[ np.float64 ] = np.loadtxt( os.path.join( data_root, filename ),
+                                                          dtype=float,
+                                                          delimiter=',' )
+    
+    # Intentional deformation of the mesh
+    ptsCoord[:,0][ptsCoord[:,0] == 0.5 ] = 0.2
+    ptsCoord[:,2][ptsCoord[:,2] == 0.5 ] = 0.7
+
+    cellPtsCoords: list[ npt.NDArray[ np.float64 ] ] = [
+            ptsCoord[ i:i + nbPtsCell ] for i in range( 0, ptsCoord.shape[ 0 ], nbPtsCell )
+        ]
+    nbCells: int = int( ptsCoord.shape[ 0 ] / nbPtsCell )
+    cellTypes = nbCells * [ VTK_TETRA ]
+    mesh: vtkUnstructuredGrid = createMultiCellMesh( cellTypes,
+                                                    cellPtsCoords )
+    
+    return mesh
+
+def __get_dataset( meshName ) -> vtkUnstructuredGrid:
+    # Get the dataset from external vtk file
+    if meshName == "polydata":
+        reader: vtkXMLUnstructuredGridReader = vtkXMLUnstructuredGridReader()
+        vtkFilename: str = "data/triangulatedSurface.vtu"
+
+    datapath: str = os.path.join( os.path.dirname( os.path.realpath( __file__ ) ), vtkFilename )
+    reader.SetFileName( datapath )
+    reader.Update()
+
+    return reader.GetOutput()
 
 
 def __generate_test_data() -> Iterator[ TestCase ]:
@@ -78,28 +118,34 @@ def __generate_test_data() -> Iterator[ TestCase ]:
 
     Yields:
         Iterator[ TestCase ]: Iterator on test cases
-    """
+    """    
     for meshName, cellType, qualityMetrics, cellTypeCounts, metricsSummary in zip( meshName_all,
                                                                                    cellTypes_all,
                                                                                    qualityMetrics_all,
                                                                                    cellTypeCounts_all,
                                                                                    metricsSummary_all,
                                                                                    strict=True ):
-        yield TestCase( meshName, cellType, qualityMetrics, cellTypeCounts, metricsSummary )
+        mesh: vtkUnstructuredGrid
+        if meshName == "tetra_mesh":
+            mesh = __get_tetra_dataset()
+        else:
+            mesh = __get_dataset( meshName )
+
+        yield TestCase( mesh, cellType, qualityMetrics, cellTypeCounts, metricsSummary )
 
 
 ids: list[ str ] = [ os.path.splitext( name )[ 0 ] for name in meshName_all ]
+    
 
 
 @pytest.mark.parametrize( "test_case", __generate_test_data(), ids=ids )
-def test_MeshQualityEnhanced( test_case: TestCase, dataSetTest: vtkUnstructuredGrid ) -> None:
+def test_MeshQualityEnhanced( test_case: TestCase ) -> None:
     """Test of CellTypeCounterEnhanced filter.
 
     Args:
         test_case (TestCase): Test case
-        dataSetTest: vtkUnstructuredGrid
     """
-    mesh: vtkUnstructuredGrid = dataSetTest( test_case.meshName )
+    mesh = test_case.mesh
     filter: MeshQualityEnhanced = MeshQualityEnhanced()
     filter.SetInputDataObject( mesh )
     if test_case.cellType == VTK_TRIANGLE: