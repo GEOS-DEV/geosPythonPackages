2632df86f6581a1f3d9f06002031d55312c8303d
Romain Baville <romain.baville@external.totalenergies.com>
2025-08-06 11:32:22 +0200

    Clean the doc and the variables names

--- geos-pv/src/geos/pv/plugins/PVCreateConstantAttributePerRegion.py
+++ geos-pv/src/geos/pv/plugins/PVCreateConstantAttributePerRegion.py
@@ -33,23 +33,23 @@ from geos.pv.utils.config import update_paths
 
 update_paths()
 
-from geos.mesh.processing.CreateConstantAttributePerRegion import CreateConstantAttributePerRegion
+from geos.mesh.processing.CreateConstantAttributePerRegion import CreateConstantAttributePerRegion, vnp, np
 
 __doc__ = """
-PVCreateConstantAttributePerRegion is a paraview Plugin that allows to create an attribute
-with constant values for each chosen indexes of a reference/region attribute.
-The region attribute has to have one component and the created attribute has one component.
-Region indexes, values and values types are choose by the user, if other region indexes exist
-values are set to nan for float type, -1 for int type or 0 for uint type.
+PVCreateConstantAttributePerRegion is a Paraview plugin that allows to create an attribute
+with constant values per components for each chosen indexes of a reference/region attribute.
+If other region indexes exist values are set to nan for float type, -1 for int type or 0 for uint type.
 
-Input and output meshes are either vtkMultiBlockDataSet or vtkDataSet.
+Input mesh is either vtkMultiBlockDataSet or vtkDataSet and the region attribute must have one component.
+The relation index/values is given by a dictionary. Its keys are the indexes and its items are the list of values for each component.
 
 To use it:
 
 * Load the module in Paraview: Tools>Manage Plugins...>Load new>PVCreateConstantAttributePerRegion.
 * Select the mesh you want to create the attributes and containing a region attribute.
 * Select the filter Create Constant Attribute Per Region in filter|0- Geos Pre-processing.
-* Set variables (region attribute, value type, attribute name, index and its value) and Apply.
+* Choose the region attribute, the relation index/values, the new attribute name, the type of the value, the number of components and their names.
+* Apply.
 
 """
 
@@ -71,39 +71,134 @@ class PVCreateConstantAttributePerRegion( VTKPythonAlgorithmBase ):
                           nOutputPorts=1,
                           inputType="vtkDataObject",
                           outputType="vtkDataObject" )
+        
+        self.clearDictRegionValues: bool = True
 
-        self.dictRegion: dict[ Any, Any ] = {}
-        self.clearDictRegion: bool = True
-        self.regionName: str = "Region"
+        # Region attribute settings.
+        self.regionName: str = ""
+        self.dictRegionValues: dict[ Any, Any ] = {}
+
+        # New attribute settings.
         self.newAttributeName: str = "newAttribute"
-        self.valueType: int = 10
+        self.valueNpType: type = np.float32
+        self.nbComponents: int = 1
+        self.componentNames: tuple[ str, ... ] = ()
+
+        # Use the handler of paraview for the log.
         self.speHandler: bool = True
+
     
+    # Settings of the attribute with the region indexes:
+    @smproperty.stringvector(
+        name="ChooseRegionAttribute",
+        label="Attribute with region indexes",
+        default_values="Choose an attribute",
+        number_of_elements="1",
+        element_types="2",
+    )
+    @smdomain.xml("""
+        <ArrayListDomain
+            name="array_list"
+            attribute_type="Scalars"
+            input_domain_name="inputs_array">
+            <RequiredProperties>
+                <Property name="Input" function="Input"/>
+            </RequiredProperties>
+        </ArrayListDomain>
+        <Documentation>
+            Select the attribute to consider as the region attribute containing the indexes of the regions.
+        </Documentation>
+        <Hints>
+            <NoDefault />
+        </Hints>
+    """ )
+    def _setRegionAttributeName( self: Self, regionName: str ) -> None:
+        """Set region attribute name.
+        
+        Args:
+            regionName (str): The name of the attribute to consider as the region attribute.
+        """
+        self.regionName = regionName
+        self.Modified()
+
+
+    @smproperty.xml("""
+        <StringVectorProperty
+            name="AttributeTable"
+            number_of_elements="2"
+            command="_setDictRegionValues"
+            repeat_command="1"
+            number_of_elements_per_command="2">
+            <Documentation>
+                Set the value of the new attribute for each region indexes, use a space between the value of each components:\n
+                    valueRegionIndex | valueComponent1 valueComponent2 ...\n
+                If the region attribute has other indexes than those given, a default value is use:\n
+                    0 for uint type, -1 for int type and nan for float type.
+            </Documentation>     
+            <Hints>
+                <AllowRestoreDefaults />
+                <ShowComponentLabels>
+                    <ComponentLabel component="0" label="Region Indexes"/>
+                    <ComponentLabel component="1" label="New Attribute Values"/>
+                </ShowComponentLabels>
+            </Hints>
+        </StringVectorProperty>
+    """ )
+    def _setDictRegionValues( self: Self, regionIndex: str, value: str ) -> None:
+        """Set the the dictionary with the region indexes and its corresponding list of value for each components.
+
+        Args:
+            regionIndex (str): Region index of the region attribute to consider.
+            value (str): List of value to use for the regionIndex. If multiple components use a coma between the value of each component.
+        """
+        if self.clearDictRegionValues:
+            self.dictRegionValues = {}
+            self.clearDictRegionValues = False
+        
+        if regionIndex != None and value != None :
+            self.dictRegionValues[ regionIndex ] = list( value.split( "," ) )
+ 
+        self.Modified()
+
+
+    @smproperty.xml( """
+        <PropertyGroup 
+            label="Settings of the attribute with the region indexes"
+            panel_visibility="default">
+            <Property name="ChooseRegionAttribute"/>
+            <Property name="AttributeTable"/>
+        </PropertyGroup>""" )
+    def _groupeRegionAttributeSettingsWidgets( self: Self ) -> None:
+        """Group the widgets to set the settings of the region attribute."""
+        self.Modified()
+
+
+    # Settings of the new attribute:
     @smproperty.xml( """
         <StringVectorProperty
             name="AttributeName"
-            command="a02SetAttributeName"
+            label="The name of the new attribute:"
             default_values="newAttribute"
             number_of_elements="1"
             element_types="2">
             <Documentation>
-                Name of the new attribute
+                Name of the new attribute to create.
             </Documentation>
         </StringVectorProperty>
-        """ )
-    def a02SetAttributeName( self: Self, value: str ) -> None:
+    """ )
+    def _setAttributeName( self: Self, newAttributeName: str ) -> None:
         """Set attribute name.
 
         Args:
-            value (str): attribute name.
+            newAttributeName (str): Name of the new attribute to create.
         """
-        if self.newAttributeName != value:
-            self.newAttributeName = value
+        self.newAttributeName = newAttributeName
         self.Modified()
+
 
     @smproperty.intvector(
         name="ValueType",
-        label="Values type",
+        label="The type of the values:",
         number_of_elements=1,
         default_values=10,
         panel_visibility="default",
@@ -120,112 +215,85 @@ class PVCreateConstantAttributePerRegion( VTKPythonAlgorithmBase ):
             <Entry value="17" text="uint64"/>
             <Entry value="10" text="float32"/>
             <Entry value="11" text="float64"/>
-            <Entry value="12" text="ID_TYPE_CODE ( int32 | int64 )"/>
         </EnumerationDomain>
         <Documentation>
-            The values type of the attribute. Each type is encoded by a int using the vtk typecode.
+            The wanted numpy scalar type for values of the new attribute.
         </Documentation>
-        """ )
-    def a02IntSingle( self: Self, value: int ) -> None:
-        """Define an input int field.
+    """ )
+    def _setValueType( self: Self, valueType: int ) -> None:
+        """Set the type for the value used to create the new attribute.
 
         Args:
-            value (int): Input
+            valueType (int): The type for the value encoding with the vtk typecode.
         """
-        if value != self.valueType:
-            self.valueType = value
+        dictType: dict[ int, Any ] = vnp.get_vtk_to_numpy_typemap()
+        self.valueNpType: type = dictType[ valueType ]
         self.Modified()
-    
+
+
+    @smproperty.intvector(
+        name="NumberOfComponents",
+        label="Number of components:",
+        number_of_elements=1,
+        default_values=1,
+        panel_visibility="default",
+    )
+    @smdomain.xml( """
+        <Documentation>
+            The number of components for the new attribute to create.
+        </Documentation>
+    """ )
+    def _setNbComponent( self: Self, nbComponents: int ) -> None:
+        """Set the number of components of the attribute to create.
+
+        Args:
+            nbComponents (int): Number of components for the new attribute.
+        """
+        self.nbComponents = nbComponents
+        self.Modified()
+
+
+    @smproperty.stringvector(
+        name="ComponentNames",
+        label="Names of components:",
+        number_of_elements=1,
+        default_values="Change if multiple components",
+        panel_visibility="default",
+    )
+    @smdomain.xml( """
+        <Documentation>
+            Names of components if multiple for the new attribute to create.
+            Use the coma and a space between each component name:\n
+                Names of components: X Y Z
+        </Documentation>
+        """ )
+    def _setComponentNames( self: Self, componentNames: str ) -> None:
+        """Set the names of the components of the attribute to create.
+
+        Args:
+            componentNamesStr (str): Names of component for the new attribute. Use a coma between each component names.
+        """
+        if componentNames == "" or componentNames == "Change if multiple components" or self.nbComponents == 1:
+            self.componentNames = ()
+        else:
+            self.componentNames = tuple( componentNames.split( "," ) )
+        
+        self.Modified()
+
+
     @smproperty.xml( """
         <PropertyGroup 
             label="Settings of the new attribute"
             panel_visibility="default">
             <Property name="AttributeName"/>
             <Property name="ValueType"/>
+            <Property name="NumberOfComponents"/>
+            <Property name="ComponentNames"/>
         </PropertyGroup>""" )
-    def b02GroupFlow1( self: Self ) -> None:
-        """Organize groups."""
+    def _groupNewAttributeSettingsWidgets( self: Self ) -> None:
+        """Group the widgets to set the settings of the new attribute."""
         self.Modified()
 
-    @smproperty.stringvector(
-        name="ChooseRegionAttribute",
-        label="Attribute with region indexes",
-        command="a01SetRegionAttributeName",
-        default_values="Choose an attribute",
-        number_of_elements="1",
-        element_types="2",
-        )
-    @smdomain.xml("""
-        <ArrayListDomain
-            name="array_list"
-            attribute_type="Scalars"
-            input_domain_name="inputs_array">
-            <RequiredProperties>
-                <Property name="Input" function="Input"/>
-            </RequiredProperties>
-        </ArrayListDomain>
-        <Documentation>
-            Select an attribute containing the indexes of the regions
-        </Documentation>
-        <Hints>
-            <NoDefault />
-        </Hints>
-    """ )
-    def a01SetRegionAttributeName( self: Self, name: str ) -> None:
-        """Set region attribute name."""
-        if self.regionName != name:
-            self.regionName = name
-        self.Modified()
-
-    @smproperty.xml("""
-        <StringVectorProperty
-            name="AttributeTable"
-            command="b01SetAttributeValues"
-            number_of_elements="2"
-            repeat_command="1"
-            number_of_elements_per_command="2">
-            <Hints>
-                <AllowRestoreDefaults />
-                <ShowComponentLabels>
-                    <ComponentLabel component="0" label="Region Indexes"/>
-                    <ComponentLabel component="1" label="New Attribute Constant Values Associated"/>
-                </ShowComponentLabels>
-            </Hints>
-            <Documentation>
-                Set the constant value of the new attribute for each region indexes.
-            </Documentation>
-        </StringVectorProperty>
-    """ )
-    def b01SetAttributeValues( self: Self, regionIndex: str, value: str ) -> None:
-        """Set the constant value of the new attribute for each region indexes.
-
-        Args:
-            regionIndex (int): Region index.
-            value (float): Attribute constant value for the regionIndex.
-        """
-        if self.clearDictRegion:
-            self.dictRegion = {}
-            self.clearDictRegion = False
-        
-        if regionIndex != None and value != None :
-            assert "," not in regionIndex, "Use the '.' not the ',' for decimal numbers"
-            assert "," not in value, "Use the '.' not the ',' for decimal numbers"
-            regionIndex = float( regionIndex )
-            value = float( value )
-            if regionIndex not in self.dictRegion.keys():
-                self.dictRegion[regionIndex] = value
-        self.Modified()
-
-    @smproperty.xml( """
-        <PropertyGroup 
-            label="Settings of the attribute with the region indexes"
-            panel_visibility="default">
-            <Property name="ChooseRegionAttribute"/>
-            <Property name="AttributeTable"/>
-        </PropertyGroup>""" )
-    def b02GroupFlow( self: Self ) -> None:
-        """Organize groups."""
-        self.Modified()
 
     def RequestDataObject(
         self: Self,
@@ -251,6 +319,7 @@ class PVCreateConstantAttributePerRegion( VTKPythonAlgorithmBase ):
             outInfoVec.GetInformationObject( 0 ).Set( outData.DATA_OBJECT(), outData )
         return super().RequestDataObject( request, inInfoVec, outInfoVec )  # type: ignore[no-any-return]
 
+
     def RequestData(
             self: Self,
             request: vtkInformation,  # noqa: F841
@@ -274,21 +343,21 @@ class PVCreateConstantAttributePerRegion( VTKPythonAlgorithmBase ):
         assert outputMesh is not None, "Output pipeline is null."
 
         outputMesh.ShallowCopy( inputMesh )
-
         filter: CreateConstantAttributePerRegion = CreateConstantAttributePerRegion( outputMesh,
                                                                                      self.regionName,
+                                                                                     self.dictRegionValues,
                                                                                      self.newAttributeName,
-                                                                                     self.dictRegion,
-                                                                                     self.valueType,
-                                                                                     self.speHandler, )
-        if self.speHandler and not filter.logger.hasHandlers():
+                                                                                     self.valueNpType,
+                                                                                     self.nbComponents,
+                                                                                     self.componentNames,
+                                                                                     self.speHandler,
+        )
+
+        if not filter.logger.hasHandlers():
             filter.setLoggerHandler( VTKHandler() )
-               
+        
         filter.applyFilter()
-        outputMesh.ShallowCopy( filter.mesh )
 
         self.clearDictRegion = True
-        self.speHandler = False
 
         return 1
-
