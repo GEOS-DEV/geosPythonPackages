ccc313c8d24adaab3be421c92aaddc0b7cb70f86
alexbenedicto <alexandre.benedicto@external.totalenergies.com>
2025-08-25 15:43:56 -0700

    Update filter to respect repository implementation

--- geos-mesh/src/geos/mesh/fault/DMZFinder.py
+++ geos-mesh/src/geos/mesh/fault/DMZFinder.py
@@ -1,48 +1,152 @@
+# ------------------------------------------------------------------------------------------------------------
+# SPDX-License-Identifier: LGPL-2.1-only
+#
+# Copyright (c) 2016-2024 Lawrence Livermore National Security LLC
+# Copyright (c) 2018-2024 TotalEnergies
+# Copyright (c) 2018-2024 The Board of Trustees of the Leland Stanford Junior University
+# Copyright (c) 2023-2024 Chevron
+# Copyright (c) 2019-     GEOS/GEOSX Contributors
+# Copyright (c) 2019-     INRIA project-team Makutu
+# All rights reserved
+#
+# See top level LICENSE, COPYRIGHT, CONTRIBUTORS, NOTICE, and ACKNOWLEDGEMENTS files for details.
+# ------------------------------------------------------------------------------------------------------------
 import numpy as np
-from vtkmodules.vtkCommonDataModel import vtkDataSet, vtkUnstructuredGrid
+from typing_extensions import Self
+from vtkmodules.vtkCommonDataModel import vtkUnstructuredGrid
 from vtkmodules.numpy_interface import dataset_adapter as dsa
-from vtkmodules.util.vtkAlgorithm import VTKPythonAlgorithmBase
 from geos.mesh.utils.genericHelpers import find_2D_cell_ids, find_cells_near_faces
+from geos.utils.Logger import logging, Logger, getLogger
+
+__doc__ = """
+Find Damage/Fractured Zone (DMZ) cells in mesh based on proximity to fault faces.
+
+Input mesh is vtkUnstructuredGrid and output is the same mesh with added DMZ array.
+
+The DMZ is identified by finding cells within a specified distance from fault faces
+in a given active region.
+
+To use a handler of yours for the logger, set the variable 'speHandler' to True and add it to the filter
+with the member function setLoggerHandler.
+
+To use it:
+
+.. code-block:: python
+
+    from geos.mesh.fault.DMZFinder import DMZFinder
+
+    # Filter inputs.
+    unstructuredGrid: vtkUnstructuredGrid
+    region_array_name: str = "attribute"
+    active_region_id: int = 0
+    active_fault_id: int = 0
+    output_array_name: str = "isDMZ"
+    dmz_length: float = 100.0
+    # Optional inputs.
+    speHandler: bool
+
+    # Instantiate the filter.
+    filter: DMZFinder = DMZFinder( unstructuredGrid, region_array_name, active_region_id,
+                                   active_fault_id, output_array_name, dmz_length, speHandler )
+
+    # Set the handler of yours (only if speHandler is True).
+    yourHandler: logging.Handler
+    filter.setLoggerHandler( yourHandler )
+
+    # Do calculations.
+    filter.applyFilter()
+"""
+
+loggerTitle: str = "DMZ Finder"
 
 
-class DMZFinder( VTKPythonAlgorithmBase ):
+class DMZFinder:
 
-    def __init__( self ) -> None:
-        super().__init__( nInputPorts=1,
-                          nOutputPorts=1,
-                          inputType='vtkUnstructuredGrid',
-                          outputType='vtkUnstructuredGrid' )
-        self._region_array_name: str = "attribute"  # Default name for region in GEOS
-        self._active_region_id: int = 0  # Default id for active region attribute in GEOS
-        self._active_fault_id: int = 0  # Default id for active fault attribute in GEOS
-        self._output_array_name: str = "isDMZ"  # New CellArray to create with value 0 if cell not in DMZ, 1 if in DMZ
-        self._dmz_len: float = 100.0
+    def __init__(
+        self: Self,
+        unstructuredGrid: vtkUnstructuredGrid,
+        region_array_name: str = "attribute",
+        active_region_id: int = 0,
+        active_fault_id: int = 0,
+        output_array_name: str = "isDMZ",
+        dmz_length: float = 100.0,
+        speHandler: bool = False,
+    ) -> None:
+        """Find Damage/Fractured Zone (DMZ) cells in mesh.
 
-    def RequestData( self, request, inInfo, outInfo ) -> None:
-        input_vtk_grid = vtkUnstructuredGrid.GetData( inInfo[ 0 ] )
-        input_grid = dsa.WrapDataObject( input_vtk_grid )
-        output_grid = dsa.WrapDataObject( vtkDataSet.GetData( outInfo ) )
+        Args:
+            unstructuredGrid (vtkUnstructuredGrid): The mesh where to find DMZ cells.
+            region_array_name (str, optional): Name of the region array. Defaults to "attribute".
+            active_region_id (int, optional): ID of the active region. Defaults to 0.
+            active_fault_id (int, optional): ID of the active fault. Defaults to 0.
+            output_array_name (str, optional): Name of the output DMZ array. Defaults to "isDMZ".
+            dmz_length (float, optional): Distance for DMZ identification. Defaults to 100.0.
+            speHandler (bool, optional): True to use a specific handler, False to use the internal handler.
+                Defaults to False.
+        """
+        self.unstructuredGrid: vtkUnstructuredGrid = unstructuredGrid
+        self.region_array_name: str = region_array_name
+        self.active_region_id: int = active_region_id
+        self.active_fault_id: int = active_fault_id
+        self.output_array_name: str = output_array_name
+        self.dmz_length: float = dmz_length
+
+        # Logger.
+        self.logger: Logger
+        if not speHandler:
+            self.logger = getLogger( loggerTitle, True )
+        else:
+            self.logger = logging.getLogger( loggerTitle )
+            self.logger.setLevel( logging.INFO )
+
+    def setLoggerHandler( self: Self, handler: logging.Handler ) -> None:
+        """Set a specific handler for the filter logger.
+
+        In this filter 4 log levels are used, .info, .error, .warning and .critical,
+        be sure to have at least the same 4 levels.
+
+        Args:
+            handler (logging.Handler): The handler to add.
+        """
+        if not self.logger.hasHandlers():
+            self.logger.addHandler( handler )
+        else:
+            self.logger.warning( "The logger already has a handler, to use yours set the argument 'speHandler' to True "
+                                 "during the filter initialization." )
+
+    def applyFilter( self: Self ) -> bool:
+        """Apply the DMZ finding algorithm to the mesh.
+
+        Returns:
+            bool: True if calculation successfully ended, False otherwise.
+        """
+        self.logger.info( f"Apply filter { self.logger.name }." )
+
+        input_grid = dsa.WrapDataObject( self.unstructuredGrid )
 
         if not self._check_inputs( input_grid ):
-            return 0
+            self.logger.error( f"The filter { self.logger.name } failed." )
+            return False
 
         # Get the array that defines the geological regions
-        region_array = input_grid.CellData[ self._region_array_name ]
+        region_array = input_grid.CellData[ self.region_array_name ]
 
-        all_mesh_face_ids: set[ int ] = find_2D_cell_ids( input_vtk_grid )
+        all_mesh_face_ids: set[ int ] = find_2D_cell_ids( self.unstructuredGrid )
         if not all_mesh_face_ids:
-            print( "No 2D face cells found." )
-            return 0
+            self.logger.error( "No 2D face cells found." )
+            self.logger.error( f"The filter { self.logger.name } failed." )
+            return False
 
-        number_cells: int = input_vtk_grid.GetNumberOfCells()
+        number_cells: int = self.unstructuredGrid.GetNumberOfCells()
         # identify which faces are in the active region
         all_faces_mask = np.zeros( number_cells, dtype=bool )
         all_faces_mask[ list( all_mesh_face_ids ) ] = True
-        active_region_mask = ( region_array == self._active_region_id )
-        active_fault_mask = ( region_array == self._active_fault_id )
+        active_region_mask = ( region_array == self.active_region_id )
+        active_fault_mask = ( region_array == self.active_fault_id )
         active_fault_faces_mask = np.logical_and( all_faces_mask, active_fault_mask )
         active_fault_faces: set[ int ] = set( np.where( active_fault_faces_mask )[ 0 ] )
-        cells_near_faces: set[ int ] = find_cells_near_faces( input_vtk_grid, active_fault_faces, self._dmz_len )
+        cells_near_faces: set[ int ] = find_cells_near_faces( self.unstructuredGrid, active_fault_faces,
+                                                              self.dmz_length )
 
         # Identify which cells are in the DMZ
         dmz_mask = np.zeros( number_cells, dtype=bool )
@@ -53,58 +157,36 @@ class DMZFinder( VTKPythonAlgorithmBase ):
         new_region_id_array = np.zeros( region_array.shape, dtype=int )
         new_region_id_array[ final_mask ] = 1
 
-        # Add new isDMZ array to the output_grid
-        output_grid.ShallowCopy( input_vtk_grid )
-        output_grid.CellData.append( new_region_id_array, self._output_array_name )
+        # Add new isDMZ array to the input_grid (which modifies the original mesh)
+        input_grid.CellData.append( new_region_id_array, self.output_array_name )
 
-        return 1
+        self.logger.info( f"The filter { self.logger.name } succeed." )
+        return True
 
-    def _check_inputs( self, dsa_mesh: dsa.UnstructuredGrid ) -> bool:
-        for attr in self.__dict__.keys():
-            if self.__dict__[ attr ] is None:
-                print( f"Error: {attr} is not set." )
-                return False
+    def _check_inputs( self: Self, dsa_mesh: dsa.UnstructuredGrid ) -> bool:
+        """Check if inputs are valid.
 
-        if self._output_array_name == self._region_array_name:
-            print( "Error: Output array name cannot be the same as region array name." )
+        Args:
+            dsa_mesh (dsa.UnstructuredGrid): The wrapped mesh to check.
+
+        Returns:
+            bool: True if inputs are valid, False otherwise.
+        """
+        if self.output_array_name == self.region_array_name:
+            self.logger.error( "Output array name cannot be the same as region array name." )
             return False
 
         if dsa_mesh is None:
-            print( "Error: Input mesh is not set." )
+            self.logger.error( "Input mesh is not set." )
             return False
 
-        region_array = dsa_mesh.CellData[ self._region_array_name ]
+        region_array = dsa_mesh.CellData[ self.region_array_name ]
         if region_array is None:
-            print( f"Error: Region array '{self._region_array_name}' is not found in input mesh." )
+            self.logger.error( f"Region array '{self.region_array_name}' is not found in input mesh." )
             return False
         else:
-            if self._active_region_id not in region_array:
-                print( f"Error: Active region ID '{self._active_region_id}' is not found in region array." )
+            if self.active_region_id not in region_array:
+                self.logger.error( f"Active region ID '{self.active_region_id}' is not found in region array." )
                 return False
 
         return True
-
-    def SetActiveFaultID( self, value: int ) -> None:
-        if self._active_fault_id != value:
-            self._active_fault_id = value
-            self.Modified()
-
-    def SetActiveRegionID( self, value: int ) -> None:
-        if self._active_region_id != value:
-            self._active_region_id = value
-            self.Modified()
-
-    def SetDmzLength( self, value: float ) -> None:
-        if self._dmz_len != value:
-            self._dmz_len = value
-            self.Modified()
-
-    def SetOutputArrayName( self, name: str ) -> None:
-        if self._output_array_name != name:
-            self._output_array_name = name
-            self.Modified()
-
-    def SetRegionArrayName( self, name: str ) -> None:
-        if self._region_array_name != name:
-            self._region_array_name = name
-            self.Modified()
