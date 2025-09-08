5b051e8eb76182a49409ba75e063b2cb8b4e47b6
jacques franc <jacquesfrancdev@gmail.com>
2025-09-08 10:38:02 +0200

    name conv

--- geos-mesh/src/geos/mesh/processing/clipToMainFrame.py
+++ geos-mesh/src/geos/mesh/processing/clipToMainFrame.py
@@ -21,8 +21,8 @@ def transform_mesh( input: vtkUnstructuredGrid, logger: logging.Logger | None = 
         logging.basicConfig( level=logging.WARNING )
         logger = logging.getLogger( __name__ )
 
-    translation, theta, axis = recenter_and_rotate( dsa.numpy_support.vtk_to_numpy( input.GetPoints().GetData() ),
-                                                    logger )
+    translation, theta, axis = __recenter_and_rotate( dsa.numpy_support.vtk_to_numpy( input.GetPoints().GetData() ),
+                                                      logger )
 
     logger.info( f"Theta {theta}" )
     logger.info( f"Axis {axis}" )
@@ -44,7 +44,7 @@ def transform_mesh( input: vtkUnstructuredGrid, logger: logging.Logger | None = 
     return transformFilter.GetOutput()
 
 
-def local_frame( pts: np.ndarray ) -> tuple[ np.ndarray, np.ndarray, np.ndarray ]:
+def __local_frame( pts: np.ndarray ) -> tuple[ np.ndarray, np.ndarray, np.ndarray ]:
     """Find a local frame for a set of points
     Args:
         pts (np.ndarray): points to find the local frame of
@@ -54,8 +54,7 @@ def local_frame( pts: np.ndarray ) -> tuple[ np.ndarray, np.ndarray, np.ndarray 
     # find 3 orthogonal vectors
     # we assume points are on a box
     # first vector is along x axis
-    ori, _, _ = origin_bounding_box( pts )
-    assert ( ( ori == np.asarray( [ 0, 0, 0 ] ) ).all() )
+    ori, _, _ = __origin_bounding_box( pts )
     u = pts[ 1 ]
     v = u
     for pt in pts[ 2: ]:  # As we assume points are on a box there should be one orthogonal to u
@@ -67,7 +66,7 @@ def local_frame( pts: np.ndarray ) -> tuple[ np.ndarray, np.ndarray, np.ndarray 
 
 
     # utils
-def origin_bounding_box( pts: np.ndarray ) -> tuple[ np.ndarray, np.ndarray, np.ndarray ]:
+def __origin_bounding_box( pts: np.ndarray ) -> tuple[ np.ndarray, np.ndarray, np.ndarray ]:
     """Find the bounding box of a set of points
     Args:
         pts (np.ndarray): points to find the bounding box of
@@ -80,7 +79,7 @@ def origin_bounding_box( pts: np.ndarray ) -> tuple[ np.ndarray, np.ndarray, np.
     return ( pts[ 0 ], pt0, pt1 )
 
 
-def recenter_and_rotate( pts: np.ndarray, logger: logging.Logger ) -> tuple[ np.ndarray, float, np.ndarray ]:
+def __recenter_and_rotate( pts: np.ndarray, logger: logging.Logger ) -> tuple[ np.ndarray, float, np.ndarray ]:
     """Recenter and rotate points to align with principal axes
     
     Args:
@@ -88,14 +87,14 @@ def recenter_and_rotate( pts: np.ndarray, logger: logging.Logger ) -> tuple[ np.
         logger (logging.logger, optional): logger instance.
     """
     # find bounding box
-    org, vmin, vmax = origin_bounding_box( pts )
+    org, vmin, vmax = __origin_bounding_box( pts )
     logger.info( f"Bounding box is {org}, {vmin}, {vmax}" )
 
     # Transformation
     translation = org
     pts -= translation
 
-    u, v, w = local_frame( pts )
+    u, v, w = __local_frame( pts )
     logger.info( f"Local frame u {u}, v {v}, w {w}" )
 
     # find rotation R = U sig V
@@ -119,21 +118,3 @@ def recenter_and_rotate( pts: np.ndarray, logger: logging.Logger ) -> tuple[ np.
 
     # return translation, rotation, pts
     return translation, theta, axis
-
-
-# #TO DO : remove
-# if __name__ == "__main__":
-
-#     reader = vtk.vtkXMLUnstructuredGridReader()
-#     reader.SetFileName("/home/jfranc/codes/python/geosPythonPackages/geos-mesh/src/geos/mesh/processing/input.vtu")
-#     reader.Update()
-
-#     # Transformation
-#     output = transform_mesh(reader.GetOutput())
-
-#     output.SetCells(VTK_HEXAHEDRON, reader.GetOutput().GetCells())
-#     writer = vtk.vtkXMLUnstructuredGridWriter()
-#     writer.SetFileName("./output.vtu")
-#     writer.SetInputData(output)
-#     writer.Update()
-#     writer.Write()
