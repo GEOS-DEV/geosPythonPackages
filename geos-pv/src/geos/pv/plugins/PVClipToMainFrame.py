633e8102f5d9eb0f92d514e06d526ce215bce223
jacques franc <jacquesfrancdev@gmail.com>
2025-09-12 16:37:58 +0200

    placeholders

---
+++ geos-pv/src/geos/pv/plugins/PVClipToMainFrame.py
@@ -0,0 +1,50 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright 2023-2024 TotalEnergies.
+# SPDX-FileContributor: Jacques Franc
+# ruff: noqa: E402 # disable Module level import not at top of file
+import sys
+from pathlib import Path
+
+from paraview.util.vtkAlgorithm import (  # type: ignore[import-not-found]
+    VTKPythonAlgorithmBase, smdomain, smhint, smproperty, smproxy,
+) # source: https://github.com/Kitware/ParaView/blob/master/Wrapping/Python/paraview/util/vtkAlgorithm.py
+from paraview.detail.loghandler import (  # type: ignore[import-not-found]
+    VTKHandler,
+) # source: https://github.com/Kitware/ParaView/blob/master/Wrapping/Python/paraview/detail/loghandler.py
+
+from vtkmodules.vtkCommonDataModel import (
+    vtkMultiBlockDataSet, )
+
+from vtkmodules.vtkCommonCore import (
+    vtkInformation,
+    vtkInformationVector,
+)
+
+# update sys.path to load all GEOS Python Package dependencies
+geos_pv_path: Path = Path( __file__ ).parent.parent.parent.parent.parent
+sys.path.insert( 0, str( geos_pv_path / "src" ) )
+from geos.pv.utils.config import update_paths
+
+update_paths()
+
+from geos.mesh.processing.ClipToMainFrame import ClipToMainFrame
+
+__doc__ = """
+Clip the input mesh to the main frame applying the correct LandmarkTransform
+
+To use it:
+
+* Load the module in Paraview: Tools>Manage Plugins...>Load new>PVClipToMainFrame.
+* Apply.
+
+"""
+
+@smproxy.filter( name="PVFillPartialArrays", label="Fill Partial Arrays" )
+@smhint.xml( '<ShowInMenu category="4- Geos Utils"/>' )
+@smproperty.input( name="Input", port_index=0 )
+@smdomain.datatype(
+    dataTypes=[ "vtkMultiBlockDataSet" ],
+    composite_data_supported=True,
+)
+class PVClipToMainFrame( VTKPythonAlgorithmBase ):
+    pass
