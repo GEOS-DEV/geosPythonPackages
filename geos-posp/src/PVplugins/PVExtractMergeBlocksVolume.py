e7447e42ef2a1d1839c8f5381c9fd13a8ab04c32
Romain Baville <romain.baville@external.totalenergies.com>
2025-09-26 14:04:44 +0200

    Move GeosBlockExtractor to geos-mesh

--- geos-posp/src/PVplugins/PVExtractMergeBlocksVolume.py
+++ geos-posp/src/PVplugins/PVExtractMergeBlocksVolume.py
@@ -22,7 +22,7 @@ from geos.utils.GeosOutputsConstants import (
     getAttributeToTransferFromInitialTime,
 )
 from geos.utils.Logger import ERROR, INFO, Logger, getLogger
-from geos_posp.filters.GeosBlockExtractor import GeosBlockExtractor
+from geos.mesh.processing.GeosBlockExtractor import GeosBlockExtractor
 from geos_posp.filters.GeosBlockMerge import GeosBlockMerge
 from geos.mesh.utils.arrayModifiers import (
     copyAttribute,
