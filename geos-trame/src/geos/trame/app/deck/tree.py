dd53ea45935f153f1c95d227926f109c14babefd
jacques franc <jacquesfrancdev@gmail.com>
2025-10-17 14:47:49 +0200

    start working on gantt + start date

--- geos-trame/src/geos/trame/app/deck/tree.py
+++ geos-trame/src/geos/trame/app/deck/tree.py
@@ -4,6 +4,7 @@
 import os
 from collections import defaultdict
 from typing import Any
+from datetime import timedelta, datetime
 
 import dpath
 import funcy
@@ -33,6 +34,7 @@ class DeckTree( object ):
         self.root = None
         self.input_has_errors = False
         self._sm_id = sm_id
+        self.world_origin_time = datetime(1924,3,28). strftime("%Y-%m-%d")# Total start date !!
 
     def set_input_file( self, input_filename: str ) -> None:
         """Set a new input file.
@@ -134,7 +136,8 @@ class DeckTree( object ):
             item: dict[ str, str | int ] = {
                 "id": global_id,
                 "summary": e.name,
-                "start_date": e.begin_time,
+                "start_date": ( datetime.strptime(self.world_origin_time,"%Y-%m-%d") + timedelta(seconds=float(e.begin_time)) ).strftime("%Y-%m-%d"),
+                "duration" : str( timedelta(seconds=float(e.end_time) - float(e.begin_time)) )
             }
             timeline.append( item )
             global_id = global_id + 1
--- geos-trame/src/geos/trame/app/ui/timeline.py
+++ geos-trame/src/geos/trame/app/ui/timeline.py
@@ -2,6 +2,7 @@
 # SPDX-FileCopyrightText: Copyright 2023-2024 TotalEnergies.
 # SPDX-FileContributor: Lionel Untereiner
 from typing import Any
+from datetime import datetime, timedelta
 
 from trame.widgets import gantt
 from trame.widgets import vuetify3 as vuetify
@@ -10,6 +11,8 @@ from trame_simput import get_simput_manager
 from geos.trame.app.deck.tree import DeckTree
 
 
+date_fmt = "%Y-%m-%d"
+                
 class TimelineEditor( vuetify.VCard ):
 
     def __init__( self, source: DeckTree, **kwargs: Any ) -> None:
@@ -18,6 +21,7 @@ class TimelineEditor( vuetify.VCard ):
 
         self.tree = source
         self.simput_manager = get_simput_manager( id=self.state.sm_id )
+        self.state.change("set_start_date")(self._set_start_date)
 
         items = self.tree.timeline()
 
@@ -31,14 +35,14 @@ class TimelineEditor( vuetify.VCard ):
             "start_date": {
                 "label": "Start",
                 "component": "gantt-date",
-                "width": 75,
+                "width": 175,
                 "placeholder": "Start",
                 "sort": "date",
             },
             "end_date": {
                 "label": "End",
                 "component": "gantt-date",
-                "width": 75,
+                "width": 0,
                 "placeholder": "End",
                 "sort": "date",
             },
@@ -56,34 +60,42 @@ class TimelineEditor( vuetify.VCard ):
                 label="Select starting simulation date",
                 prepend_icon="",
                 prepend_inner_icon="$calendar",
-                placeholder="09/18/2024",
+                # placeholder="09/18/2024",
+                v_model="start_date",
             )
             vuetify.VDivider()
-            with (
-                    vuetify.VContainer( "Events timeline" ),
-                    vuetify.VTimeline(
-                        direction="horizontal",
-                        truncate_line="both",
-                        align="center",
-                        side="end",
-                    ),
-                    vuetify.VTimelineItem( v_for=( f"item in {items}", ), key="i", value="item", size="small" ),
-            ):
-                vuetify.VAlert( "{{ item.summary }}" )
-                vuetify.Template( "{{ item.start_date }}", raw_attrs=[ "v-slot:opposite" ] )
+            # with (
+            #         vuetify.VContainer( "Events timeline" ),
+            #         vuetify.VTimeline(
+            #             direction="horizontal",
+            #             truncate_line="both",
+            #             align="center",
+            #             side="end",
+            #         ),
+            #         vuetify.VTimelineItem( v_for=( f"item in {items}", ), key="i", value="item", size="small" ),
+            # ):
+            #     vuetify.VAlert( "{{ item.summary }}" )
+            #     vuetify.Template( "{{ item.start_date }}", raw_attrs=[ "v-slot:opposite" ] )
 
             with vuetify.VContainer( "Events chart" ):
                 gantt.Gantt(
                     canEdit=True,
-                    dateLimit=30,
-                    startDate="2024-11-01 00:00",
-                    endDate="2024-12-01 00:00",
+                    dateLimit=40,
+                    startDate= self.state.start_date if self.state.start_date else self.tree.world_origin_time,
+                    endDate=(datetime.strptime( self.state.start_date if self.state.start_date else self.tree.world_origin_time,date_fmt) + timedelta(days=40)).strftime(date_fmt),
                     # title='Gantt-pre-test',
                     fields=fields,
                     update=( self.update_from_js, "items" ),
                     items=( "items", items ),
                     classes="fill_height",
                 )
+
+    def _set_start_date(self, start_date : str | None, **_: Any) -> None:
+        if start_date is None:
+            start_date = self.tree.world_origin_time.strftime(date_fmt)
+            return
+        
+        self.state.start_date = start_date
 
     def update_from_js( self, *items: tuple ) -> None:
         """Update method called from javascript."""
