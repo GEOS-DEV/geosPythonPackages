ed2f42fbe7a92d01b6f24492c4fc1e8ed4e8914c
Jacques Franc <49998870+jafranc@users.noreply.github.com>
2025-11-13 10:35:32 +0100

    mypy-ification of fix

--- geos-trame/src/geos/trame/app/deck/tree.py
+++ geos-trame/src/geos/trame/app/deck/tree.py
@@ -286,10 +286,7 @@ class DeckTree( object ):
 
             if proxy_name.isnumeric() and int( proxy_name ) < len( model_copy ):
                 models.append( ( proxy_name, model_copy ) )
-                if is_list:
-                    proxy_name = int(proxy_name)
-                #won't work if is_list # TO DO IMMEDIATELY -- review proxy strat
-                model_copy = model_copy[ proxy_name ]
+                model_copy = model_copy[ int(proxy_name) if is_list else proxy_name ]
                 continue
 
             if proxy_name in model_copy:
