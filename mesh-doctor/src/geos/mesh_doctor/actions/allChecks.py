c6d3e8468c940ca63b0221353ffff9a82b28ade7
alexbenedicto <alexandre.benedicto@external.totalenergies.com>
2025-11-19 17:13:00 -0800

    Use meshAction instead of action in allChecks and mainChecks to avoid redundant reading of input file

--- mesh-doctor/src/geos/mesh_doctor/actions/allChecks.py
+++ mesh-doctor/src/geos/mesh_doctor/actions/allChecks.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 from typing import Any
+from vtkmodules.vtkCommonDataModel import vtkUnstructuredGrid
 from geos.mesh_doctor.parsing.cliParsing import setupLogger
 from geos.mesh_doctor.register import __loadModuleAction
+from geos.mesh.io.vtkIO import readUnstructuredGrid
 
 
 @dataclass( frozen=True )
@@ -16,6 +18,26 @@ class Result:
     checkResults: dict[ str, Any ]
 
 
+def meshAction( mesh: vtkUnstructuredGrid, options: Options ) -> Result:
+    """Performs all checks available on a loaded mesh.
+
+    Args:
+        mesh (vtkUnstructuredGrid): The loaded mesh to analyze.
+        options (Options): The options for processing.
+
+    Returns:
+        Result: The result of all checks performed.
+    """
+    checkResults: dict[ str, Any ] = {}
+    for checkName in options.checksToPerform:
+        checkMeshAction = __loadModuleAction( checkName, "meshAction" )
+        setupLogger.info( f"Performing check '{checkName}'." )
+        option = options.checksOptions[ checkName ]
+        checkResult = checkMeshAction( mesh, option )
+        checkResults[ checkName ] = checkResult
+    return Result( checkResults=checkResults )
+
+
 def action( vtuInputFile: str, options: Options ) -> Result:
     """Reads a vtu file and performs all checks available on it.
 
@@ -26,11 +48,5 @@ def action( vtuInputFile: str, options: Options ) -> Result:
     Returns:
         Result: The result of all checks performed.
     """
-    checkResults: dict[ str, Any ] = {}
-    for checkName in options.checksToPerform:
-        checkAction = __loadModuleAction( checkName )
-        setupLogger.info( f"Performing check '{checkName}'." )
-        option = options.checksOptions[ checkName ]
-        checkResult = checkAction( vtuInputFile, option )
-        checkResults[ checkName ] = checkResult
-    return Result( checkResults=checkResults )
+    mesh: vtkUnstructuredGrid = readUnstructuredGrid( vtuInputFile )
+    return meshAction( mesh, options )
