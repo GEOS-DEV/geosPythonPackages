5b7a7eca4c12c3a61180a22258d0206934d0f0fc
Romain Baville <romain.baville@external.totalenergies.com>
2025-11-28 11:11:40 +0100

    Create attribute piece enum

---
+++ geos-utils/src/geos/utils/attributePieceEnum.py
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright 2023-2024 TotalEnergies.
+# SPDX-FileContributor: Romain Baville
+from enum import Enum
+
+class AttributePiece( Enum ):
+    POINTS = "points"
+    CELLS = "cells"
