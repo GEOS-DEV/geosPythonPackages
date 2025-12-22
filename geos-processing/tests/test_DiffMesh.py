1b3dc366db0973bc6a7d8b0b58a7b88633cc7632
Romain Baville <romain.baville@external.totalenergies.com>
2025-12-22 14:18:28 +0100

    test diffFields

---
+++ geos-processing/tests/test_DiffMesh.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright 2023-2024 TotalEnergies.
+# SPDX-FileContributor: Romain Baville
+# SPDX-License-Identifier: Apache 2.0
+# ruff: noqa: E402 # disable Module level import not at top of file
+# mypy: disable-error-code="operator"
+import pytest
+
+from typing import Any
+from vtkmodules.vtkCommonDataModel import vtkMultiBlockDataSet
+
+from geos.processing.generic_processing_tools.DiffFields import DiffFields
